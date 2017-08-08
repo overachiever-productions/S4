a2dfc491f1a56362b40f26361e404ee04d35b828
Michael K. Campbell <mike@overachiever.net>
2017-08-08 14:39:15 -0700

    Bugfix to dba_SplitString
    Version 3.7.1.16638 (3.7 is for t-log management/automation).
    Fixed bug with dba_SplitString where it was using sys.columns - which only amounted to roughly 1100 rows total on many servers.
    Corrected by:
    a) using sys.all_objects
    b) cross join sys.all_objects
    c) limiting with TOP (@delimiter) - where @delimiter is LEN(@serialized) + 1000.

--- Common/dba_SplitString.sql
+++ Common/dba_SplitString.sql
@@ -26,14 +26,19 @@ RETURNS @Results TABLE (result nvarchar(200))
 AS 
 	BEGIN
 
-	-- Version 3.5.0.16602	
+	-- Version 3.7.1.16638	
 	-- License/Code/Details/Docs: https://git.overachiever.net/Repository/Tree/00aeb933-08e0-466e-a815-db20aa979639  (username: s4   password: simple )
 	
+	DECLARE @MaxLength int;
+	SET @MaxLength = LEN(@serialized) + 1000;
+
 	SET @serialized = @delimiter + @serialized + @delimiter;
 
 	WITH tally AS ( 
-		SELECT TOP (2000) ROW_NUMBER() OVER (ORDER BY [name]) AS N
-		FROM sys.columns
+		SELECT TOP (@MaxLength) 
+			ROW_NUMBER() OVER (ORDER BY o1.[name]) AS N
+		FROM sys.all_objects o1 
+		CROSS JOIN sys.all_objects o2
 	)
 
 	INSERT INTO @Results (result)
