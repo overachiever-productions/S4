50d1643378f190e654676c120ec4e86e7a941ce3
Michael K. Campbell <mike@overachiever.net>
2017-12-21 10:00:19 -0800

    4.1.1.16773 Added signatures.sql
    Simple 'reference' for execution signatures.

---
+++ About/signatures.sql
@@ -0,0 +1,49 @@
+
+/*
+	v 4.0.1.16756
+		Signatures.
+
+
+*/
+
+
+EXEC dbo.backup_databases
+    @BackupType = NULL, 
+    @DatabasesToBackup = N'', 
+    @DatabasesToExclude = N'', 
+    @Priorities = N'', 
+    @BackupDirectory = N'', 
+    @CopyToBackupDirectory = N'', 
+    @BackupRetention = N'', 
+    @CopyToRetention = N'', 
+    @RemoveFilesBeforeBackup = NULL, 
+    @EncryptionCertName = NULL, 
+    @EncryptionAlgorithm = NULL, 
+    @AddServerNameToSystemBackupPath = NULL, 
+    @AllowNonAccessibleSecondaries = NULL, 
+    @LogSuccessfulOutcomes = NULL, 
+    @OperatorName = NULL, 
+    @MailProfileName = NULL, 
+    @EmailSubjectPrefix = N'', 
+    @PrintOnly = NULL; 
+
+
+
+EXEC dbo.restore_databases
+    @DatabasesToRestore = N'', 
+    @DatabasesToExclude = N'', 
+    @Priorities = N'', 
+    @BackupsRootPath = N'', 
+    @RestoredRootDataPath = N'', 
+    @RestoredRootLogPath = N'', 
+    @RestoredDbNamePattern = N'', 
+    @AllowReplace = N'', 
+    @SkipLogBackups = NULL, 
+    @CheckConsistency = NULL, 
+    @DropDatabasesAfterRestore = NULL, 
+    @MaxNumberOfFailedDrops = 0, 
+    @OperatorName = NULL, 
+    @MailProfileName = NULL, 
+    @EmailSubjectPrefix = N'', 
+    @PrintOnly = NULL; 
+
