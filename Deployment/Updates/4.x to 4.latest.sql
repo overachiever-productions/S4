948ad94d319e254940f3dea33632caef65b81bc2
Michael K. Campbell <mike@overachiever.net>
2018-01-25 12:39:34 -0800

    Update to Deployments scripts (adding copy_database)
    4.2.1.16808

--- Deployment/Updates/4.x to 4.latest.sql
+++ Deployment/Updates/4.x to 4.latest.sql
@@ -69,7 +69,7 @@ GO
 
 ----------------------------------------------------------------------------------------
 -- Version 4.2.0.16786 Rollup: 
-DECLARE @targetVersion varchar(20) = '4.2.0.16786';
+DECLARE @targetVersion varchar(20) = '4.2.1.16808';
 IF NOT EXISTS(SELECT NULL FROM dbo.version_history WHERE version_number = @targetVersion) BEGIN
 	
 	PRINT 'Deploying v4.2 Updates.... ';
@@ -1575,6 +1575,117 @@ GO
 
 
 
+USE admindb;
+GO
+
+
+IF OBJECT_ID('dbo.copy_database','P') IS NOT NULL
+	DROP PROC dbo.copy_database;
+GO
+
+CREATE PROC dbo.copy_database 
+	@SourceDatabaseName			sysname, 
+	@TargetDatabaseName			sysname, 
+	@BackupsRootPath			sysname	= N'<BackupsRootPath, sysname, E:\SQLBackups>', 
+	@DataPath					sysname = N'<DataPath, sysname, D:\SQLData>', 
+	@LogPath					sysname = N'<LogPath, sysname, D:\SQLData>',
+	@OperatorName				sysname = N'<OperatorName, sysname, Alerts>',
+	@MailProfileName			sysname = N'<MailProfileName, sysname, General>'
+AS
+	SET NOCOUNT ON; 
+
+	-- License/Code/Details/Docs: https://git.overachiever.net/Repository/Tree/00aeb933-08e0-466e-a815-db20aa979639  (username: s4   password: simple )
+
+	IF NULLIF(@SourceDatabaseName,'') IS NULL BEGIN
+		RAISERROR('@SourceDatabaseName cannot be Empty/NULL. Please specify the name of the database you wish to copy (from).', 16, 1);
+		RETURN -1;
+	END;
+
+	IF NULLIF(@TargetDatabaseName, '') IS NULL BEGIN
+		RAISERROR('@TargetDatabaseName cannot be Empty/NULL. Please specify the name of new database that you want to create (as a copy).', 16, 1);
+		RETURN -1;
+	END;
+
+	-- Make sure the target database doesn't already exist: 
+	IF EXISTS (SELECT NULL FROM sys.databases WHERE [name] = @TargetDatabaseName) BEGIN
+		RAISERROR('@TargetDatabaseName already exists as a database. Either pick another target database name - or drop existing target before retrying.', 16, 1);
+		RETURN -5;
+	END;
+
+	PRINT N'Attempting to Restore a backup of [' + @SourceDatabaseName + N'] as [' + @TargetDatabaseName + N']';
+	
+	DECLARE @restored bit = 0;
+	DECLARE @errorMessage nvarchar(MAX); 
+
+	BEGIN TRY 
+		EXEC admindb.dbo.restore_databases
+			@DatabasesToRestore = @SourceDatabaseName,
+			@BackupsRootPath = @BackupsRootPath,
+			@RestoredRootDataPath = @DataPath,
+			@RestoredRootLogPath = @LogPath,
+			@RestoredDbNamePattern = @TargetDatabaseName,
+			@SkipLogBackups = 0,
+			@CheckConsistency = 0, 
+			@DropDatabasesAfterRestore = 0,
+			@OperatorName = @OperatorName, 
+			@MailProfileName = @MailProfileName, 
+			@EmailSubjectPrefix = N'[COPY DATABASE OPERATION] : ';
+
+	END TRY
+	BEGIN CATCH
+		SET @errorMessage = ISNULL(@errorMessage, '') + N'Unexpected Exception while restoring copy of database. Error: ' + CAST(ERROR_NUMBER() AS nvarchar(30)) + N' - ' + ERROR_MESSAGE() + N' ';
+	END CATCH
+
+	-- 'sadly', restore_databases does a great job of handling most exceptions during execution - meaning that if we didn't get errors, that doesn't mean there weren't problems. So, let's check up: 
+	IF EXISTS (SELECT NULL FROM sys.databases WHERE [name] = @TargetDatabaseName AND state_desc = N'ONLINE')
+		SET @restored = 1; -- success (the db wasn't there at the start of this sproc, and now it is (and it's online). 
+	ELSE BEGIN 
+		-- then we need to grab the latest error: 
+		SELECT @errorMessage = error_details FROM dbo.restore_log WHERE restore_test_id = (
+			SELECT MAX(restore_test_id) FROM dbo.restore_log WHERE test_date = GETDATE() AND [database] = @SourceDatabaseName AND restored_as = @TargetDatabaseName);
+
+		IF @errorMessage IS NULL -- hmmm weird:
+			SET @errorMessage = N'Unknown error with restore operation - execution did NOT complete as expected. Please Check Email for additional details/insights.';
+
+	END
+
+	IF @errorMessage IS NULL
+		PRINT N'Restore Complete. Kicking off backup [' + @TargetDatabaseName + N'].';
+	ELSE BEGIN
+		PRINT @errorMessage;
+		RETURN -10;
+	END;
+	
+	DECLARE @backedUp bit = 0;
+	IF @restored = 1 BEGIN
+		
+		BEGIN TRY
+			EXEC admindb.dbo.backup_databases
+				@BackupType = N'FULL',
+				@DatabasesToBackup = @TargetDatabaseName,
+				@BackupDirectory = @BackupsRootPath,
+				@BackupRetention = N'24h',
+				@OperatorName = @OperatorName, 
+				@MailProfileName = @MailProfileName, 
+				@EmailSubjectPrefix = N'[COPY DATABASE OPERATION] : ';
+
+			SET @backedUp = 1;
+		END TRY
+		BEGIN CATCH
+			SET @errorMessage = ISNULL(@errorMessage, '') + N'Unexpected Exception while executing backup of new/copied database. Error: ' + CAST(ERROR_NUMBER() AS nvarchar(30)) + N' - ' + ERROR_MESSAGE() + N' ';
+		END CATCH
+
+	END;
+
+	IF @restored = 1 AND @backedUp = 1 
+		PRINT N'Operation Complete.';
+	ELSE BEGIN
+		PRINT N'Errors occurred during execution:';
+		PRINT @errorMessage;
+	END;
+
+	RETURN 0;
+GO
 
 
 
