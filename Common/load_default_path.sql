cf7c6a0ed25ac93dcdde011c22a635d77494e466
Michael K. Campbell <mike@overachiever.net>
2018-02-19 12:21:24 -0800

    Adding load_default_path.
    Standardizing and streamlining registry lookup details (after attempting to query from sys.dm_server_registry) via xp_instance_regread to extract default paths for backups, data, and log files via SQL Server settings/registry.
    
    4.3.1.16833

---
+++ Common/load_default_path.sql
@@ -0,0 +1,59 @@
+
+
+
+/*
+
+
+
+	NOTES:
+		- It'd be great to select path info from sys.dm_server_registry
+			https://docs.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-server-registry-transact-sql
+
+			only... that doesn't cover any of the path info we want/need. (which is lame.) 
+			So.... xp_instance_regread is the only real option.
+
+
+*/
+
+
+
+USE admindb;
+GO
+
+
+IF OBJECT_ID('dbo.load_default_path','FN') IS NOT NULL
+	DROP FUNCTION dbo.load_default_path;
+GO
+
+CREATE FUNCTION dbo.load_default_path(@PathType sysname) 
+RETURNS nvarchar(4000)
+AS
+BEGIN 
+	DECLARE @output sysname;
+
+	IF UPPER(@PathType) = N'BACKUPS'
+		SET @PathType = N'BACKUP';
+	DECLARE @valueName nvarchar(4000);
+
+	SET @valueName = CASE @PathType
+		WHEN N'BACKUP' THEN N'BackupDirectory'
+		WHEN N'DATA' THEN N'DefaultData'
+		WHEN N'LOG' THEN N'DefaultLog'
+		ELSE N''
+	END;
+
+	IF @valueName = N''
+		RETURN 'Error. Invalid @PathType Specified.';
+
+	EXEC master..xp_instance_regread
+		N'HKEY_LOCAL_MACHINE',  
+		N'Software\Microsoft\MSSQLServer\MSSQLServer',  
+		@valueName,
+		@output OUTPUT, 
+		'no_output'
+
+	RETURN @output;
+END;
+GO
+
+
