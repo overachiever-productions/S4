f1883d1e4cdb684669e016d85ccaaee8d9667888
Michael K. Campbell <mike@overachiever.net>
2018-03-30 16:38:24 -0700

    Checkin of audit checkup/reporting logic
    v4.6.6.16872

---
+++ S4 Audits/generate_server_audit_signature.sql
@@ -0,0 +1,116 @@
+
+/*
+
+
+	SAMPLE Execution: 
+		EXEC [dbo].[generate_server_audit_signature] 'Server Audit';
+
+		or... 
+		DECLARE @sig bigint = 0; 
+		EXEC dbo.generate_server_audit_signature 'Server Audit', @AuditSignature = @sig OUTPUT;
+		SELECT @sig;
+
+*/
+
+
+
+
+
+-- decent enough idea... only... .. hmmm (was going to say, can't use this inside of server_synchronization_checks - as I can't... because i'd need RPC perms on the PARTNER. 
+--			but... hmmm. yeah... still sort of makes sense with being able to set up a hash for a spec/audit 'on the fly' so that it can be used to 'lock' or 'bind' a job that checks up?
+USE [admindb];
+GO
+
+IF OBJECT_ID('dbo.generate_server_audit_signature','P') IS NOT NULL
+	DROP PROC dbo.generate_server_audit_signature;
+GO
+
+CREATE PROC dbo.generate_server_audit_signature 
+	@AuditName						sysname, 
+	@IncludeAuditGUIDInHash			bit	= 0, 
+	@AuditSignature					bigint = NULL OUTPUT
+AS
+	SET NOCOUNT ON; 
+	DECLARE @errorMessage nvarchar(MAX);
+
+	DECLARE @hashes table ( 
+		hash bigint NOT NULL
+	);
+
+	DECLARE @hash int = 0;
+	DECLARE @auditID int; 
+	DECLARE @auditGUID uniqueidentifier;
+
+	SELECT @auditID = audit_id, @auditGUID = [audit_guid] FROM sys.[server_audits] WHERE [name] = @AuditName;
+	IF @auditID IS NULL BEGIN 
+		SET @errorMessage = N'Specified Server Audit [' + @AuditName + N'] does NOT exist. Please check input and try again.';
+		RAISERROR(@errorMessage, 16, 1);
+		RETURN -1;
+	END;
+
+	IF @IncludeAuditGUIDInHash = 1
+		SELECT @hash = CHECKSUM([name], [audit_guid], [type], [on_failure], [is_state_enabled], [queue_delay], [predicate]) FROM sys.[server_audits];
+	ELSE 
+		SELECT @hash = CHECKSUM([name], [type], [on_failure], [is_state_enabled], [queue_delay], [predicate]) FROM sys.[server_audits];
+	INSERT INTO @hashes ([hash]) VALUES (CAST(@hash AS bigint));
+
+	IF EXISTS (SELECT NULL FROM sys.[server_audits] WHERE [name] = @AuditName AND [type] = 'FL') BEGIN
+		SELECT @hash = CHECKSUM(max_file_size, max_files, reserve_disk_space, log_file_path) FROM sys.[server_file_audits] WHERE [audit_id] = @auditID;  -- note, log_file_name will always be different because of the GUIDs. 
+		INSERT INTO @hashes ([hash]) VALUES (CAST(@hash AS bigint));
+	END
+
+	SET @hash = 0;
+	SELECT 
+		@hash = @hash + CHECKSUM([name], [is_state_enabled])
+	FROM 
+		sys.[server_audit_specifications] 
+	WHERE 
+		[audit_guid] = @auditGuid;
+
+	INSERT INTO @hashes ([hash]) VALUES (CAST(@hash AS bigint));
+
+	DECLARE details CURSOR LOCAL FAST_FORWARD FOR 
+	SELECT 
+		[sasd].[audit_action_id], 
+		[sasd].[class], 
+		[sasd].[major_id],
+		[sasd].[minor_id], 
+		[sasd].[audited_principal_id], 
+		[sasd].[audited_result], 
+		[sasd].[is_group]
+	FROM
+		sys.[server_audit_specification_details] sasd 
+		INNER JOIN sys.[server_audit_specifications] sas ON sasd.[server_specification_id] = sas.[server_specification_id]
+	WHERE 
+		sas.[audit_guid] = @auditGUID
+	ORDER BY 
+		[sasd].[audit_action_id];
+		
+
+	DECLARE @auditActionID char(4), @class tinyint, @majorId int, @minorInt int, @principal int, @result nvarchar(60), @isGroup bit; 
+
+	OPEN [details]; 
+	FETCH NEXT FROM [details] INTO @auditActionID, @class, @majorId, @minorInt, @principal, @result, @isGroup;
+
+	WHILE @@FETCH_STATUS = 0 BEGIN 
+
+		SELECT @hash = CHECKSUM(@auditActionID, @class, @majorId, @minorInt, @principal, @result, @isGroup)
+		INSERT INTO @hashes ([hash]) VALUES (CAST(@hash AS bigint));
+
+		FETCH NEXT FROM [details] INTO @auditActionID, @class, @majorId, @minorInt, @principal, @result, @isGroup;
+	END;
+
+	CLOSE [details];
+	DEALLOCATE [details];
+
+	IF @AuditSignature IS NULL
+		SELECT SUM([hash]) [audit_signature] FROM @hashes; 
+	ELSE	
+		SELECT @AuditSignature = SUM(hash) FROM @hashes;
+
+	RETURN 0;
+GO
+
+
+
+
---
+++ S4 Audits/verify_server_audit_configuration.sql
@@ -0,0 +1,86 @@
+
+/*
+
+
+
+
+	Sample Execution/Example:
+		EXEC verify_server_audit_configuration 'Server Audit', -34135575811, @printOnly = 1;
+
+*/
+
+IF OBJECT_ID('dbo.verify_server_audit_configuration','P') IS NOT NULL
+	DROP PROC dbo.verify_server_audit_configuration;
+GO
+
+CREATE PROC dbo.verify_server_audit_configuration 
+	@AuditName				sysname, 
+	@OptionalAuditHash		bigint = NULL, 
+	@EmailSubjectPrefix		nvarchar(50)		= N'[Audit Configuration] ',
+	@MailProfileName		sysname				= N'General',	
+	@OperatorName			sysname				= N'Alerts',	
+	@PrintOnly				bit					= 0	
+AS
+	SET NOCOUNT ON; 
+
+	DECLARE @errorMessage nvarchar(MAX);
+
+	DECLARE @errors table (
+		error_id int IDENTITY(1,1) NOT NULL, 
+		error nvarchar(MAX) NOT NULL
+	);
+
+	-- make sure audit exists and is enabled. 
+	DECLARE @auditID int; 
+	DECLARE @isEnabled bit;
+
+	SELECT @auditID = audit_id, @isEnabled = is_state_enabled FROM sys.[server_audits] WHERE [name] = @AuditName;
+	IF @auditID IS NULL BEGIN 
+		SELECT @errorMessage = N'WARNING: Server Audit [' + @AuditName + N'] does not currently exist on [' + @@SERVERNAME + N'].';
+		INSERT INTO @errors([error]) VALUES (@errorMessage);
+	END;
+
+	IF @isEnabled = 0 BEGIN 
+		SELECT @errorMessage = N'WARNING: Server Audit [' + @AuditName + N'] on [' + @@SERVERNAME + N'] is currently NOT enabled.';
+		INSERT INTO @errors([error]) VALUES (@errorMessage);
+	END;
+
+	-- if there's a checksum, verify that they match. 
+	IF @OptionalAuditHash IS NOT NULL BEGIN
+		DECLARE @auditSignature bigint = 0;
+
+		EXEC admindb.dbo.generate_server_audit_signature @AuditName, @AuditSignature = @auditSignature OUTPUT;
+
+		IF @auditSignature <> 0 AND (@OptionalAuditHash <> @auditSignature) BEGIN
+			SELECT @errorMessage = N'WARNING: Server Audit [' + @AuditName + N'] on [' + @@SERVERNAME + N'] does NOT match signature defined via call to admindb.dbo.verify_server_audit_configuration. (If you recently made an audit/audit specification change, review audit change logs then regenerate @OptionalAuditHash using admindbo.generate_server_audit_hash.)';
+			INSERT INTO @errors([error]) VALUES (@errorMessage);
+		END
+	END;
+
+	IF EXISTS (SELECT NULL FROM	@errors) BEGIN 
+		DECLARE @subject nvarchar(MAX) = @EmailSubjectPrefix + N' - Synchronization Problems Detected';
+		DECLARE @crlf nchar(2) = NCHAR(13) + NCHAR(10);
+		DECLARE @tab nchar(1) = NCHAR(9);
+
+		SET @errorMessage = N'The following errors were detected: ' + @crlf;
+
+		SELECT @errorMessage = @errorMessage + @tab + N'- ' + error + @crlf
+		FROM @errors
+		ORDER BY error_id;
+
+		IF @PrintOnly = 1 BEGIN
+			PRINT N'SUBJECT: ' + @subject;
+			PRINT N'BODY: ' + @errorMessage;
+		  END
+		ELSE BEGIN 
+			EXEC msdb..sp_notify_operator 
+				@profile_name = @MailProfileName, 
+				@name = @OperatorName, 
+				@subject = @Subject, 
+				@body = @errorMessage;	
+		END;
+	END;
+
+	RETURN 0;
+GO
+
