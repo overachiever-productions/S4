c2b67243390ea5826ceae170ddbfa442e2422d8a
Michael K. Campbell <mike@overachiever.net>
2018-03-30 16:37:48 -0700

    File Move of Server Audit.sql (to setup/config)
    4.6.6.16872

--- S4 Audits/Setup and Configuration/Server Audit.sql
+++ S4 Audits/Setup and Configuration/Server Audit.sql
@@ -3,13 +3,25 @@
 
 /*
 
-
+	FODDER:
+		semi-practical example of a 'filter'. (semi-practical in that this shows VERY well why you'd want to and how you'd go about it, only... the example is kind of fishy as Aaron says.)
+		https://dba.stackexchange.com/questions/63760/sql-server-audit-specification-filter-dml-by-all-but-one-user
 
 	IMPORTANT:
 		- MUST set the ON_FAILURE action to be what is needed/desired. 
 				DEFAULT for this template is to FAIL_OPERATION. 
 
+	CONSIDERATIONS:
+		- In highly sensitive environments the following 2x concerns might make sense:
+			a) create an additional audit and/or (just) an additional Server Specification that includes AUDIT_CHANGE_GROUP - so that the 'main' audit tracks overall activity and such, and the 'secondary' audit (spec) is a 'watchdog that watches the wathdog'. 
+				it might even make sense to create the first audit with a target of FILE, and the secondary audit with a TARGET of the security log. 
 
+			b) could/would make sense to create a simple job that ensures that a specific audit (or whatever) is running - otherwise, send an alert to Alerts or a specific person, and so on. 
+
+
+	TODO / vNEXT: 
+		- MIGHT want to consider setting up a filter (i.e., at audit level) to squelch a particularly messy ... ALTER (AL) - update statistics... 
+			looks like it'll be a bit hard to specify/trap... and, there's some benefit to having it in place (obviously) but... damn. 
 
 */
 
@@ -43,18 +55,12 @@ WITH (
 	QUEUE_DELAY = 2000,
 	ON_FAILURE = FAIL_OPERATION  -- options: SHUTDOWN | FAIL_OPERATION | CONTINUE. My recommendation: initially START with CONTINUE, then audit failures/fix - THEN switch to FAIL_OPERATION. 
 )
-
 -- WHERE ... (any specifics can be configured here). 
 --		e.g., WHERE object_name = 'CreditCards'... etc. 
-
-
 GO
 
-
--- enable: 
 ALTER SERVER AUDIT [<audit_name, sysname, Server Audit>] WITH (STATE = ON);
 GO
-
 
 ------------------------------------------------------------------------------------------------------------------------------------
 -- Server Audit Specification: 
@@ -64,49 +70,52 @@ IF EXISTS (SELECT NULL FROM sys.[server_audit_specifications] WHERE [name] = N'<
 	DROP SERVER AUDIT SPECIFICATION [<serverAuditSpecName, sysname, Server Audit Specification>];
 END
 
-
+-- https://docs.microsoft.com/en-us/sql/relational-databases/security/auditing/sql-server-audit-action-groups-and-actions
 
 -- NOTE: this is defined for 2012+ instances. (2008 instances have entirely different audit actions/groups.)
 CREATE SERVER AUDIT SPECIFICATION [<serverAuditSpecName, sysname, Server Audit Specification>]
 	FOR SERVER AUDIT [<audit_name, sysname, Server Audit>]
-		ADD (APPLICATION_ROLE_CHANGE_PASSWORD_GROUP),
+		ADD (APPLICATION_ROLE_CHANGE_PASSWORD_GROUP), -- if not being used, no real overhead... 
 		ADD (AUDIT_CHANGE_GROUP),
 		ADD (BACKUP_RESTORE_GROUP),
-		ADD (DATABASE_CHANGE_GROUP ),
-		--ADD (DATABASE_LOGOUT_GROUP ),
-		--ADD (DATABASE_OBJECT_ACCESS_GROUP),
+		ADD (BROKER_LOGIN_GROUP), -- if broker isn't being used, we won't log any data (so no real overhead). 
+		ADD (DATABASE_MIRRORING_LOGIN_GROUP), -- only reports on TLS issues with mirroring... (and, presumably? AGs?)
+		ADD (DATABASE_CHANGE_GROUP),
+		ADD (DATABASE_LOGOUT_GROUP),	-- contained users only... 
+		--ADD (DATABASE_OBJECT_ACCESS_GROUP),  -- can lead to a LOT of overhead, and is primararily for service broker monitoring (see docs).
 		ADD (DATABASE_OBJECT_CHANGE_GROUP),
 		ADD (DATABASE_OBJECT_OWNERSHIP_CHANGE_GROUP),
-		ADD (DATABASE_OBJECT_PERMISSION_CHANGE_GROUP ),
+		ADD (DATABASE_OBJECT_PERMISSION_CHANGE_GROUP),
 		ADD (DATABASE_OPERATION_GROUP),
-		ADD (DATABASE_OWNERSHIP_CHANGE_GROUP ),
+		ADD (DATABASE_OWNERSHIP_CHANGE_GROUP),
 		ADD (DATABASE_PERMISSION_CHANGE_GROUP),
-		ADD (DATABASE_PRINCIPAL_CHANGE_GROUP ),
+		ADD (DATABASE_PRINCIPAL_CHANGE_GROUP),
 		ADD (DATABASE_PRINCIPAL_IMPERSONATION_GROUP),
-		ADD (DATABASE_ROLE_MEMBER_CHANGE_GROUP ),
+		ADD (DATABASE_ROLE_MEMBER_CHANGE_GROUP),
 		ADD (DBCC_GROUP),
-		--ADD (FAILED_DATABASE_AUTHENTICATION_GROUP),  -- contained dbs only. 
+		ADD (FAILED_DATABASE_AUTHENTICATION_GROUP),  -- contained dbs only. but, worth monitoring for failed login attempts. 
 		ADD (FAILED_LOGIN_GROUP),
-		--ADD (FULLTEXT_GROUP),
-		ADD (LOGIN_CHANGE_PASSWORD_GROUP ),
-		--ADD (LOGOUT_GROUP),
-		ADD (SCHEMA_OBJECT_ACCESS_GROUP),
+		--ADD (FULLTEXT_GROUP), -- no real 'security' info here.  
+		ADD (LOGIN_CHANGE_PASSWORD_GROUP),
+	--ADD (LOGOUT_GROUP),  -- can be enabled... connection pooling makes this a bit less vile than might originally think... 
+		--ADD (SCHEMA_OBJECT_ACCESS_GROUP), -- this includes SELECTs ... and can literally crater busier servers.
 		ADD (SCHEMA_OBJECT_CHANGE_GROUP),
 		ADD (SCHEMA_OBJECT_OWNERSHIP_CHANGE_GROUP),
-		ADD (SCHEMA_OBJECT_PERMISSION_CHANGE_GROUP ),
+		ADD (SCHEMA_OBJECT_PERMISSION_CHANGE_GROUP),
 		ADD (SERVER_OBJECT_CHANGE_GROUP),
 		ADD (SERVER_OBJECT_OWNERSHIP_CHANGE_GROUP),
-		ADD (SERVER_OBJECT_PERMISSION_CHANGE_GROUP ),
+		ADD (SERVER_OBJECT_PERMISSION_CHANGE_GROUP),
 		ADD (SERVER_OPERATION_GROUP),  -- juicy server-level changes
 		ADD (SERVER_PERMISSION_CHANGE_GROUP),
-		ADD (SERVER_PRINCIPAL_CHANGE_GROUP ),
+		ADD (SERVER_PRINCIPAL_CHANGE_GROUP),
 		ADD (SERVER_PRINCIPAL_IMPERSONATION_GROUP),
-		ADD (SERVER_ROLE_MEMBER_CHANGE_GROUP ),
-		ADD (SERVER_STATE_CHANGE_GROUP ),
-		--ADD (SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP),  -- contained only.
-		--ADD (SUCCESSFUL_LOGIN_GROUP),  -- depends upon environment... 
+		ADD (SERVER_ROLE_MEMBER_CHANGE_GROUP),
+		ADD (SERVER_STATE_CHANGE_GROUP),
+		ADD (SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP),  -- contained only.
+	--ADD (SUCCESSFUL_LOGIN_GROUP),  -- depends upon environment/scenario. Again, pooled connections mitigate problems. 
 		ADD (TRACE_CHANGE_GROUP),
-		--ADD (USER_CHANGE_PASSWORD_GROUP),  -- contained only... 
+	--ADD (TRANSACTION_GROUP) -- includes IMPLICIT transactions - which can/will dump a TON of modifications into a db. BUT, usefull when monitoring ALL modifications. 
+		ADD (USER_CHANGE_PASSWORD_GROUP),  -- contained only... 
 		ADD (USER_DEFINED_AUDIT_GROUP) -- outputs any 'custom' details defined by sp_audit_write and friends. 
 	WITH (STATE = ON);
 GO
@@ -171,57 +180,51 @@ GO
 ------------------------------------------------------------------------------------------------------------------------------------
 -- DB Audit Specifcation and Implemenation/Addition:
 
--- PER database to AUDIT: 
+-- Note: there's no real need for these - other than for what I've done above with msdb - i.e., ACTION_GROUPS are better handled at the server-specification level - because they'll 
+--		be applied to ALL databases (including newly spun-up, restored, and other types of databases). 
+--			AND, if you don't want a specific db monitored, then just (stop first and) modify the FILTERS (where clause) for the Audit itself. 
+--				e.g., 
 
--- NOTE: 2012+ semantics: 
+-- Example: 
+--ALTER SERVER AUDIT [<audit_name, sysname, Server Audit>] WITH (STATE = OFF);
 
-USE [<targetDBName, sysname, xxxx>];
-GO
+--ALTER SERVER AUDIT [<audit_name, sysname, Server Audit>] WHERE [database_name] <> 'Billing' AND [database_name] <> 'IdentityDB';
+---- at this point, double-check the audit definition (i.e., filters) to make sure they 'took'.
 
-IF EXISTS(SELECT NULL FROM [<targetDBName, sysname, xxxx>].sys.[database_audit_specifications] WHERE [name] = N'<dbSpecName, sysname, Database Audit Specification>') BEGIN 
-	ALTER DATABASE AUDIT SPECIFICATION [<dbSpecName, sysname, Database Audit Specification>] WITH (STATE = OFF);
-	DROP DATABASE AUDIT SPECIFICATION [<dbSpecName, sysname, Database Audit Specification>];
-END;
+--ALTER SERVER AUDIT [<audit_name, sysname, Server Audit>] WITH (STATE = ON);
 
-CREATE DATABASE AUDIT SPECIFICATION [<dbSpecName, sysname, Database Audit Specification>]
-	FOR SERVER AUDIT [<audit_name, sysname, Server Audit>]
 
-		--ADD (APPLICATION_ROLE_CHANGE_PASSWORD_GROUP), -- appliation role usage only... 
-		ADD (AUDIT_CHANGE_GROUP)--,
-		--ADD (BACKUP_RESTORE_GROUP),		-- duplicate of server spec (TODO: compare details of db-level vs server-level events/actions)
-		--ADD (DATABASE_CHANGE_GROUP),		-- duplicate of server spec (TODO: compare details of db-level vs server-level events/actions)
-		--ADD (DATABASE_LOGOUT_GROUP),
-		--ADD (DATABASE_OBJECT_ACCESS_GROUP),
-		--ADD (DATABASE_OBJECT_CHANGE_GROUP),
-		--ADD (DATABASE_OBJECT_OWNERSHIP_CHANGE_GROUP),
-		--ADD (DATABASE_OBJECT_PERMISSION_CHANGE_GROUP),
-		--ADD (DATABASE_OPERATION_GROUP),
-		--ADD (DATABASE_OWNERSHIP_CHANGE_GROUP),
-		--ADD (DATABASE_PERMISSION_CHANGE_GROUP),
-		--ADD (DATABASE_PRINCIPAL_CHANGE_GROUP),
-		--ADD (DATABASE_PRINCIPAL_IMPERSONATION_GROUP),
-		--ADD (DATABASE_ROLE_MEMBER_CHANGE_GROUP),
-		--ADD (DBCC_GROUP),
-		--ADD (FAILED_DATABASE_AUTHENTICATION_GROUP),
-		--ADD (SCHEMA_OBJECT_ACCESS_GROUP),
-		--ADD (SCHEMA_OBJECT_CHANGE_GROUP),
-		--ADD (SCHEMA_OBJECT_OWNERSHIP_CHANGE_GROUP),
-		--ADD (SCHEMA_OBJECT_PERMISSION_CHANGE_GROUP),
-		--ADD (SUCCESSFUL_DATABASE_AUTHENTICATION_GROUP),
-		--ADD (USER_CHANGE_PASSWORD_GROUP),
-		--ADD (USER_DEFINED_AUDIT_GROUP)
-
-	WITH (STATE = ON);
-GO
+-- NOTE:
+--		IF you create a db-level audit spec that captures 'more' than what you've scoped via the Server-Level spec (where 'more' means - either different actions/groups or things 'outside' of your filter)... you'll get those results 'merged' into the overall whole. 
+--			in other words, audits take the 'widest' amount of inputs specified (ORs instead of NOT ANDs). 
+--		FURTHER, some actions/groups that are 'global' or 'server' in scope (like DBCC commands, and the likes) will end up 'firing' even if executed in an 'excluded' database - simply because those kinds of commands don't really 'care' which DB they're run from, as they're 'server-level' in scope ANYHOW. 
 
 
 
+------------------------------------------------------------------------------------------------------------------------------------
+-- Audit Management and Oversight:
+
+-- Fine-tuning of config - i.e., see what actions are being tracked and at what rates and if there are any LEGIT filters that could be put in place that would decrease noise and NOT decrease efficacy of the audit. 
+SELECT 
+	action_id, 
+	COUNT(*) instances
+FROM 
+	fn_get_audit_file('<audit_path, sysname, D:\Audits\>*',default,default)
+GROUP BY 
+	action_id 
+ORDER BY 
+	2 DESC;
+
+-- then, look up the action_ids in question for additional details on their scope/origin/etc. 
+SELECT * FROM sys.[dm_audit_actions] WHERE [action_id] = 'SL'; -- for example, this is a 'select'....  (if you don't need SELECT across ALL databases and/or only need it for one or two tables, or one or two databases, then set up db-level specifications as 'narrowly' as possible/logical. (when in doubt, record more data).
+
+-- docs on this DMV are here: https://docs.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-audit-actions-transact-sql
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- B. Analysis/Review:
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Sample 'review' query... 
-SELECT action_id, succeeded, [event_time], server_principal_name, database_principal_name, additional_information, [statement] 
+SELECT action_id, succeeded, [event_time], server_principal_name, database_principal_name, CAST(additional_information AS xml) [additional_information], [statement] 
 FROM fn_get_audit_file('<audit_path, sysname, D:\Audits\>*',default,default)
-ORDER BY [event_time], [transaction_id], [sequence_number];
+ORDER BY [event_time] DESC, [transaction_id] DESC, [sequence_number] DESC;
