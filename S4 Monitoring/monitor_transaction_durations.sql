2dee8607446b3ffb3584c84010dbd2f3a6871966
Michael K. Campbell <mike@overachiever.net>
2018-08-04 15:34:55 -0700

    Text Cleanup.
    'truncating' sentances at shorter lengths to allow for better formatting via emails.

--- S4 Monitoring/monitor_transaction_durations.sql
+++ S4 Monitoring/monitor_transaction_durations.sql
@@ -180,36 +180,51 @@ AS
 		+ @crlf
 		+ @tab + @tab + @tab + @tab + N'EXEC admindb.dbo.list_transactions;' + @crlf 
 		+ @crlf 
-		+ @tab + @tab + N'If the same session_id is still listed and causing problems, you can attempt to KILL the session in question by running KILL X - where X is the session_id you wish to terminate. (So, if session_id 234 is causing problems, you would execute KILL 234; ' + @crlf 
-		+ @tab + @tab + N'WARNING: KILLing an in-flight/long-running transaction is NOT an immediate operation. It typically takes around 75% - 150% of the time a transaction has taken to ''roll-forward'' in order to ''KILL'' or ROLLBACK a long-running operation. ' + @crlf
-		+ @tab + @tab + @tab + N'Example: suppose it takes 10 minutes for a long-running transaction (like a large UPDATE or DELETE operation) to complete and/or get stuck - or it has been running for ~10 minutes when you attempt to KILL it.' + @crlf
-		+ @tab + @tab + @tab + @tab + N'At this point (i.e., 10 minutes into an active transaction), you should ROUGHLY expect the rollback to take anywhere from 7 - 15 minutes to execute.' + @crlf
-		+ @tab + @tab + @tab + @tab + N'NOTE: If a short/simple transaction (like running an UPDATE against a single row) executes and the gets ''orphaned'' (i.e., it somehow gets stuck and/or there was an EXPLICIT BEGIN TRAN and the operation is waiting on an explicit COMMIT), ' + @crlf
-		+ @tab + @tab + @tab + @tab + @tab + N'Then, in this case the transactional ''overhead'' should have been minimal - meaning that a KILL operation should be very QUICK and almost immediate - because you are only rolling-back a few milliseconds'' or second''s worth of transactional overhead.' + @crlf 
-		+ @tab + @tab + N'Once you KILL a session, the rollback proccess will begin (if there was a transaction in-flight). Keep checking admindb.dbo.list_transactions to see if the session in question is still running - and once it is DONE running blocked processes and other operations SHOULD start to work as normal again.' + @crlf
-		+ @tab + @tab + @tab + N'IF you would like to see ROLLBACK process you can run: KILL ### WITH STATUSONLY; and SQL Server will USUALLY (but not always) provide a relatively accurate picture of how far along the rollback is. ' + @crlf 
+		+ @tab + @tab + N'If the same session_id is still listed and causing problems, you can attempt to KILL the session in question by running ' + @crlf 
+		+ @tab + @tab + @tab + N'KILL X - where X is the session_id you wish to terminate. (So, if session_id 234 is causing problems, you would execute KILL 234; )' + @crlf 
+		+ @tab + @tab + N'WARNING: KILLing an in-flight/long-running transaction is NOT an immediate operation. It typically takes around 75% - 150% of the time a ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'transaction has taken to ''roll-forward'' in order to ''KILL'' or ROLLBACK a long-running operation. ' + @crlf
+		+ @tab + @tab + @tab + N'Example: suppose it takes 10 minutes for a long-running transaction (like a large UPDATE or DELETE operation) to complete and/or ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'GET stuck - or it has been running for ~10 minutes when you attempt to KILL it.' + @crlf
+		+ @tab + @tab + @tab + @tab + N'At this point (i.e., 10 minutes into an active transaction), you should ROUGHLY expect the rollback to take '  + @crlf
+		+ @tab + @tab + @tab + @tab + @tab + N' anywhere from 7 - 15 minutes to execute.' + @crlf
+		+ @tab + @tab + @tab + @tab + N'NOTE: If a short/simple transaction (like running an UPDATE against a single row) executes and the gets ''orphaned'' (i.e., it ' + @crlf 
+		+ @tab + @tab + @tab + @tab + @tab + N'somehow gets stuck and/or there was an EXPLICIT BEGIN TRAN and the operation is waiting on an explicit COMMIT), ' + @crlf
+		+ @tab + @tab + @tab + @tab + @tab + N'then, in this case, the transactional ''overhead'' should have been minimal - meaning that a KILL operation should be very QUICK '  + @crlf 
+		+ @tab + @tab + @tab + @tab + @tab + @tab + N'and almost immediate - because you are only rolling-back a few milliseconds'' or second''s worth of transactional overhead.' + @crlf 
 		+ @crlf
-		+ @tab + @tab + N'NOTE: If you are unable to determine the ''root'' blocker and/or are WILLING to effectively take the ENTIRE database ''down'' to fix problems with blocking/time-outs due to long-running transactions, you CAN kick the entire database in question into SINGLE_USER mode thereby forcing all ' + @crlf
+		+ @tab + @tab + N'Once you KILL a session, the rollback proccess will begin (if there was a transaction in-flight). Keep checking admindb.dbo.list_transactions to see ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'IF the session in question is still running - and once it is DONE running blocked processes and other operations SHOULD start to work as normal again.' + @crlf
+		+ @tab + @tab + @tab + N'IF you would like to see ROLLBACK process you can run: KILL ### WITH STATUSONLY; and SQL Server will USUALLY (but not always) provide a relatively accurate ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'picture of how far along the rollback is. ' + @crlf 
+		+ @crlf
+		+ @tab + @tab + N'NOTE: If you are unable to determine the ''root'' blocker and/or are WILLING to effectively take the ENTIRE database ''down'' to fix problems with blocking/time-outs ' + @crlf 
+		+ @tab + @tab + @tab + N'due to long-running transactions, you CAN kick the entire database in question into SINGLE_USER mode thereby forcing all ' + @crlf
 		+ @tab + @tab + @tab + N'in-flight transactions to ROLLBACK - at the expense of (effectively) KILLing ALL connections into the database AND preventing new connections.' + @crlf
 		+ @tab + @tab + @tab + N'As you might suspect, this is effectively a ''nuclear'' option - and can/will result in across-the-board down-time against the database in question. ' + @crlf
-		+ @tab + @tab + @tab + N'WARNING: Knocking a database into SINGLE_USER mode will NOT do ANYTHING to ''speed up'' or decrease ROLLBACK time for any transactions in flight. In fact, because it KILLs ALL transactions in the target database, it can take LONGER in some cases to ''go'' SINGLE_USER mode ' + @crlf
+		+ @tab + @tab + @tab + N'WARNING: Knocking a database into SINGLE_USER mode will NOT do ANYTHING to ''speed up'' or decrease ROLLBACK time for any transactions in flight. ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'In fact, because it KILLs ALL transactions in the target database, it can take LONGER in some cases to ''go'' SINGLE_USER mode ' + @crlf
 		+ @tab + @tab + @tab + @tab + N'than finding/KILLing a root-blocker. Likewise, taking a database into SINGLE_USER mode is a semi-advanced operation and should NOT be done lightly.' + @crlf 
 		+ @crlf 
 		+ @tab + @tab + @tab + N'To force a database into SINGLE_USER mode (and kill all connections/transactions), run the following from within the master database: ' + @crlf
 		+ @crlf 
 		+ @tab + @tab + @tab + @tab + N'ALTER DATABSE [targetDBNameHere] SET SINGLE_USER WITH ROLLBACK AFTER 5 SECONDS;' + @crlf 
 		+ @crlf 
-		+ @tab + @tab + @tab + N'The command above will allow any/all connections and transactions currently active in the target database another 5 seconds to complete - while also blocking any NEW connections into the database. After 5 seconds (and you can obvious set this value as you would like), ' + @crlf
-		+ @tab + @tab + @tab + @tab + N'all in-flight transactions will be KILLed and start the ROLLBACK process - and any active connections in the database will also be KILLed and kicked-out of the database in question.' + @crlf
-		+ @tab + @tab + @tab + N'WARNING: Once a database has been put into SINGLE_USER mode it can ONLY be accessed by the session that switched the database into SINGLE_USER mode. As such, if you CLOSE your connection/session - ''control'' of the database ''falls'' to the next session that ' + @crlf
-		+ @tab + @tab + @tab + @tab + N'accesses the database - and all OTHER connections are blocked - which means that IF you close your connection/session, you will have to ACTIVELY fight other processes for connection into the database before you can set it to MULTI_USER again - and clear it for production use.' + @crlf 
+		+ @tab + @tab + @tab + N'The command above will allow any/all connections and transactions currently active in the target database another 5 seconds to complete - while also ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'blocking any NEW connections into the database. After 5 seconds (and you can obvious set this value as you would like), all in-flight transactions ' + @crlf
+		+ @tab + @tab + @tab + @tab + N'will be KILLed and start the ROLLBACK process - and any active connections in the database will also be KILLed and kicked-out of the database in question.' + @crlf
+		+ @tab + @tab + @tab + N'WARNING: Once a database has been put into SINGLE_USER mode it can ONLY be accessed by the session that switched the database into SINGLE_USER mode. As such, if ' + @crlf 
+		+ @tab + @tab + @tab + @tab + N'you CLOSE your connection/session - ''control'' of the database ''falls'' to the next session that ' + @crlf
+		+ @tab + @tab + @tab + @tab + N'accesses the database - and all OTHER connections are blocked - which means that IF you close your connection/session, you will have to ACTIVELY fight other ' + @crlf
+		+ @tab + @tab + @tab + @tab + N'processes for connection into the database before you can set it to MULTI_USER again - and clear it for production use.' + @crlf 
 		+ @crlf 
-		+ @tab + @tab + @tab + N'Once a database has been put into SINGLE_USER mode (i.e., after the command has been executed and ALL in-flight transactions have been rolled-back and all connections have been terminated and the state of the database switches to SINGLE_USER mode), any transactional locking and blocking ' + @crlf
-		+ @tab + @tab + @tab + @tab + N'in the target database will be corrected. At which point you can then return the database to active service by switching it back to MULTI_USER mode by executing the following: ' + @crlf 
+		+ @tab + @tab + @tab + N'Once a database has been put into SINGLE_USER mode (i.e., after the command has been executed and ALL in-flight transactions have been rolled-back and all ' + @crlf
+		+ @tab + @tab + @tab + @tab + N'connections have been terminated and the state of the database switches to SINGLE_USER mode), any transactional locking and blocking in the target database' + @crlf
+		+ @tab + @tab + @tab + @tab + N'will be corrected. At which point you can then return the database to active service by switching it back to MULTI_USER mode by executing the following: ' + @crlf 
 		+ @crlf 
 		+ @tab + @tab + @tab + @tab + @tab + N'ALTER DATABASE [targetDatabaseInSINGLE_USERMode] SET MULTI_USER;' + @crlf 
 		+ @crlf 
-		+ @tab + @tab + @tab + @tab + N'Note, of course, that the command above can ONLY be executed (without throwing errors) by the session_id that currently ''owns'' the SINGLE_USER access into the database in question.' + @crlf;
+		+ @tab + @tab + @tab + @tab + N'Note that the command above can ONLY be successfully executed by the session_id that currently ''owns'' the SINGLE_USER access into the database in question.' + @crlf;
 
 	IF @PrintOnly = 1 BEGIN 
 		PRINT @message;
