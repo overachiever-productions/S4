d686dbc293e25e174b60e09f8b27f9cbb4ff493d
Michael K. Campbell <mike@overachiever.net>
2019-05-10 22:10:45 -0700

    S4-111: Addition of dbo.format_windows_login
    handles U and G type logins...

--- Common/Internal/format_windows_login.sql
+++ Common/Internal/format_windows_login.sql
@@ -26,57 +26,51 @@
             SET NOCOUNT ON;
         
         Expected Error: 
-                SELECT dbo.format_sql_login(1, N'NONE', N'', '', '0x0456598165fe', 'Billing', DEFAULT, NULL, NULL);
+                SELECT dbo.format_windows_login(1, N'NONE', N'', '0x0456598165fe', 'Billing', DEFAULT);
 
         CREATE only - with a (fake) SID and default DB of [Billing] specified
-                SELECT dbo.format_sql_login(1, DEFAULT, N'Bilbo', 'THe One Ring 1s fun.', '0x0456598165fe', 'Billing', NULL, NULL, NULL);
+                SELECT dbo.format_windows_login(1, DEFAULT, N'Bilbo', '0x0456598165fe', 'Billing', NULL);
 
         CREATE, but login defaults to NOT active... so it's disabled: 
-                SELECT dbo.format_sql_login(NULL, DEFAULT, N'Bilbo', 'THe One Ring 1s fun.', '0x0456598165fe', 'Billing', NULL, NULL, NULL);
+                SELECT dbo.format_windows_login(NULL, DEFAULT, N'Bilbo', '0x0456598165fe', 'Billing', NULL);
 
         CREATE or ALTER (if it exists - but SID can't be changed by the ALTER):
-                SELECT dbo.format_sql_login(1, N'ALTER', N'Bilbo', 'THe One Ring 1s fun.', '0x0456598165fe', 'Billing', NULL, NULL, NULL);
+                SELECT dbo.format_windows_login(1, N'ALTER', N'Bilbo', '0x0456598165fe', 'Billing', NULL);
 
         CREATE or DROP + CREATE 
-                SELECT dbo.format_sql_login(1, N'DROP_AND_CREATE', N'Bilbo', 'THe One Ring 1s fun.', '0x0456598165fe', 'Billing', NULL, NULL, NULL);
+                SELECT dbo.format_windows_login(1, N'DROP_AND_CREATE', N'Bilbo', '0x0456598165fe', 'Billing', NULL);
 
         As above, but defaults to SAFE config (i.e., @Enabled is NOT specified):
-                SELECT dbo.format_sql_login(NULL, N'DROP_AND_CREATE', N'Bilbo', 'THe One Ring 1s fun.', '0x0456598165fe', 'Billing', NULL, NULL, NULL);
-
+                SELECT dbo.format_windows_login(NULL, N'DROP_AND_CREATE', N'Bilbo', '0x0456598165fe', 'Billing', NULL);
 
 
 */
-
 USE [admindb];
+GO 
+
+IF OBJECT_ID('dbo.format_windows_login','FN') IS NOT NULL
+	DROP FUNCTION dbo.format_windows_login;
 GO
 
-
-IF OBJECT_ID('dbo.format_sql_login','FN') IS NOT NULL
-	DROP FUNCTION dbo.format_sql_login;
-GO
-
-CREATE FUNCTION dbo.format_sql_login (
+CREATE FUNCTION dbo.format_windows_login (
     @Enabled                          bit,                                  -- IF NULL the login will be DISABLED via the output/script.
     @BehaviorIfLoginExists            sysname         = N'NONE',            -- { NONE | ALTER | DROP_ANCE_CREATE }
     @Name                             sysname,                              -- always required.
-    @Password                         varchar(256),                         -- NOTE: while not 'strictly' required by ALTER LOGIN statements, @Password is ALWAYS required for dbo.format_sql_login.
     @SID                              varchar(100),                         -- only processed if this is a CREATE or a DROP/CREATE... 
     @DefaultDatabase                  sysname         = N'master',          -- have to specify DEFAULT for this to work... obviously
-    @DefaultLanguage                  sysname         = N'[DEFAULT]',       -- have to specify DEFAULT for this to work... obviously
-    @CheckExpriration                 bit             = 0,                  -- have to specify DEFAULT for this to work... obviously
-    @CheckPolicy                      bit             = 0                   -- have to specify DEFAULT for this to work... obviously
+    @DefaultLanguage                  sysname         = N'[DEFAULT]'        -- have to specify DEFAULT for this to work... obviously
 )
 RETURNS nvarchar(MAX)
 AS 
     -- {copyright}
 
     BEGIN 
+
         DECLARE @crlf nchar(2) = NCHAR(13) + NCHAR(10);
         DECLARE @newAtrributeLine sysname = @crlf + NCHAR(9) + N' ';
 
         DECLARE @output nvarchar(MAX) = N'-- ERROR scripting login. ' + @crlf 
-            + N'--' + NCHAR(9) + N'Parameters @Name and @Password are both required.' + @crlf
-            + N'--' + NCHAR(9) + '   Supplied Values: @Name -> [{Name}], @Password -> [{Password}].'
+            + N'--' + NCHAR(9) + N'Parameter @Name is required.';
         
         IF NULLIF(@BehaviorIfLoginExists, N'') IS NULL 
             SET @BehaviorIfLoginExists = N'NONE';
@@ -84,19 +78,18 @@ AS
         IF UPPER(@BehaviorIfLoginExists) NOT IN (N'NONE', N'ALTER', N'DROP_AND_CREATE')
             SET @BehaviorIfLoginExists = N'NONE';
 
-        IF (NULLIF(@Name, N'') IS NULL) OR (NULLIF(@Password, N'') IS NULL) BEGIN 
-            SET @output = REPLACE(@output, N'{name}', ISNULL(NULLIF(@Name, N''), N'#NOT PROVIDED#'));
-            SET @output = REPLACE(@output, N'{Password}', ISNULL(NULLIF(@Password, N''), N'#NOT PROVIDED#'));
-
+        IF (NULLIF(@Name, N'') IS NULL) BEGIN 
+            -- output is already set/defined.
             GOTO Done;
-        END;        
-        
-        DECLARE @attributes sysname = N'{PASSWORD}{SID}{DefaultDatabase}{DefaultLanguage}{CheckExpiration}{CheckPolicy};';
+        END;            
+
+
+DECLARE @attributes sysname = N'{SID}{DefaultDatabase}{DefaultLanguage};';
         DECLARE @alterAttributes sysname = REPLACE(@attributes, N'{SID}', N'');
 
         DECLARE @template nvarchar(MAX) = N'
 IF NOT EXISTS (SELECT NULL FROM [master].[sys].[server_principals] WHERE [name] = ''{Name}'') BEGIN 
-    CREATE LOGIN [{Name}] WITH {Attributes} {Disable} {ElseClause} {SidReplacementDrop}{CreateOrAlter} {Attributes2} {Disable2}
+    CREATE LOGIN [{Name}] FROM WINDOWS WITH {Attributes} {Disable} {ElseClause} {SidReplacementDrop}{CreateOrAlter} {Attributes2} {Disable2}
 END;
 GO
 ';
@@ -132,14 +125,7 @@ GO
         -- initialize output with basic details:
         SET @template = REPLACE(@template, N'{Attributes}', @attributes);
         SET @output = REPLACE(@template, N'{Name}', @Name);
-
-        IF (@Password LIKE '0x%') AND (@Password NOT LIKE '%HASHED')
-            SET @Password = @Password + N' HASHED';
-        ELSE 
-            SET @Password = N'''' + @Password + N'''';
         
-        SET @output = REPLACE(@output, N'{PASSWORD}', @newAtrributeLine + NCHAR(9) + N'PASSWORD = ' + @Password);
-
         IF NULLIF(@SID, N'') IS NOT NULL BEGIN 
             SET @output = REPLACE(@output, N'{SID}', @newAtrributeLine + N',SID = ' + @SID);
             END;
@@ -166,21 +152,6 @@ GO
             SET @output = REPLACE(@output, N'{DefaultLanguage}', N'');
         END;
 
-        -- checks:
-        IF @CheckExpriration IS NULL BEGIN 
-            SET @output = REPLACE(@output, N'{CheckExpiration}', N'');
-            END;
-        ELSE BEGIN 
-            SET @output = REPLACE(@output, N'{CheckExpiration}', @newAtrributeLine + N',CHECK_EXPIRATION = ' + CASE WHEN @CheckExpriration = 1 THEN N'ON' ELSE 'OFF' END);
-        END;
-
-        IF @CheckPolicy IS NULL BEGIN 
-            SET @output = REPLACE(@output, N'{CheckPolicy}', N'');
-            END;
-        ELSE BEGIN 
-            SET @output = REPLACE(@output, N'{CheckPolicy}', @newAtrributeLine + N',CHECK_EXPIRATION = ' + CASE WHEN @CheckPolicy = 1 THEN N'ON' ELSE 'OFF' END);
-        END;
-
         -- enabled:
         IF ISNULL(@Enabled, 0) = 0 BEGIN -- default secure (i.e., if we don't get an EXPLICIT enabled, disable... 
             SET @output = REPLACE(@output, N'{Disable}', @crlf + @crlf + NCHAR(9) + N'ALTER LOGIN ' + QUOTENAME(@Name) + N' DISABLE;');
@@ -192,5 +163,6 @@ GO
 Done:
 
         RETURN @output;
+
     END;
 GO
