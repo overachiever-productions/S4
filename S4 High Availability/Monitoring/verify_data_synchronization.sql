4cdbcc20c6cad4a65f34ae3455ce6c8d9864a4a4
Michael K. Campbell <mike@overachiever.net>
2019-10-08 09:18:58 -0700

    S4-232: Addressing Ghosted Record Cleanup
    Option to 'ignore' anomalous RPO gaps due to Ghosted Record Cleanup.

--- S4 High Availability/Monitoring/verify_data_synchronization.sql
+++ S4 High Availability/Monitoring/verify_data_synchronization.sql
@@ -18,6 +18,88 @@
 					https://technet.microsoft.com/en-us/library/cc917681.aspx?f=255&MSPPError=-2147217396
 
 
+
+	TODO:
+		[NOTE: the info below now correlates to this parameter: @ExcludeAnomolousSyncDeviations]
+			
+		document/address problem or issue with Ghosted Records. 
+			specifically, we're determining 'lag' and RPOs by means of the vector between primary_last_log_commit and secondary_last_log_commit
+				as, normally, they should be within a few milliseconds of each other. 
+
+			Only, imagine a scenario like the following: 
+				- very little traffic and/or very few modifications. 
+					specifically, a few INSERTs every 10-180 seconds and a FEW DELETEs every 1-2.5 minutes... 
+
+					at this point the last-log-commit time on BOTH servers is going to be, say, 10, 40, or even 120 seconds+ OLD. 
+						it'll be the SAME on both - cuz the last change was 'a long time ago' - but it happened synchronously on both servers at/around the exact same time
+							so... the VECTOR between those two metrics isn't large - but the duration between NOW and when that happened is ... large-ish. 
+
+				- Ghosted Record Cleanup. 
+					this is a process that doesn't have to be synchronously committed - it happens on the primary and (apparently?) gets thrown over the fence to the secondary
+						(I actually need to double-check and see if i can spot whether this stuff is happening or not... 
+							i.e., all of these 'details' are a very SOLID working THEORY... they're the only thing that really explains what's going on here - and I can see 
+								that ghosted record cleanup IS happening on the primary and it IS happening at/around the time of the main problem - i.e., it's the catalyst - or so it appears). 
+
+					meaning that ... 
+						we now have 'async' changes ... that are 120+ seconds or whatever apart. 
+
+						I've started to tackle this by means of averages - i.e., instead of taking the RPO 'right now' ... I 'poll' over N seconds and X times. 
+
+						BUT, that's still not enough... i.e., 4x RPOs of 0 and 1x RPO of 147 still weigh in at an average of, say, 45seconds - well over the 15s threshold i normally want. 
+
+						SO, 
+							I need to do the following: 
+								- 1. set up different options for polling - maybe EVEN by DATABASE... 
+									this'll SUCK to put into @Params for this sproc so... instead, I'm going to: 
+										a) default the @pollCount and @waitDuration to whatever makes sense (i.e., whatever's already in the sproc below). 
+										b) set up options to LOOK FOR 'overwrites' for those defaults ... in the dbo.settings table. 
+											and then... POSSIBLY, if i can think of a clean/easy way to tackle it ... look at doing this if/when certain dbs are found or whatever... 
+												(probably NOT going to be feasible). 
+
+
+								- 2. Look at using STDEV - to ignore/'remove'/'flatten' hiccups caused by this problem - as per the 'dogs' stuff here: 
+									
+										https://www.mathsisfun.com/data/standard-deviation.html
+
+											and further detailed here: 
+												https://www.mathsisfun.com/data/standard-deviation-formulas.html
+
+						NOTE: 
+							Final implementation is as follows: 
+								1. If @ExcludeAnomolousSyncDeviations is enabled... then. 
+								2. Calculate the MEAN (AVG) per each DB. 
+								3. Also calculate SAMPLE STDEV (i.e., STDEV vs STDEVP) per database. 
+								4. Add STDEV to MEAN. 
+								5. Substract STDEV from MEAN. 
+								6. _IF_ MEAN - STDEV is < 0, then 
+									ADD 
+										MEAN + STDEV + (ABS(MEAN - STDEV))
+									instead of just using STDEV + MEAN (i.e., 'overwrite 'rule' #4'... 
+
+								7. ALLOW removal/exclusion of 1x (i.e., largest) rpo or other target value IF 
+									the value of output #6 is > output of #4 AND... the RPO in question is > #6... 
+
+
+								e.g., 
+									RPOs are 
+										0
+										147 
+										0 
+										3
+										0
+
+									AVG/MEAN = 30 seconds (i.e., 150 / 5) 
+									SAMPLED MEAN = 66. 
+									30 + 66 = 96 as upper bound (#4)
+									30 - 66 = -36 as lower bound. (#5)
+									36 + 96 = 132 (#6 - i.e., outcome of #5 was < 0 - so ABS value is added to result of #4). 
+
+									147 IS > 132
+										so it's excluded/ignored - meaning we get an average of .75  (i.e., 3 seconds / 4 iterations).
+											whereas if @ExcludeAnomolousSyncDeviations had been set to 0, we'd be back at the AVG of 30 seconds. 
+
+
+
 */
 
 USE [admindb];
@@ -29,8 +111,13 @@ GO
 
 CREATE PROC dbo.verify_data_synchronization 
 	@IgnoredDatabases						nvarchar(MAX)		= NULL,
-	@RPOThreshold							sysname				= '10 seconds',
-	@RTOThreshold							sysname				= '40 seconds',
+	@RPOThreshold							sysname				= N'10 seconds',
+	@RTOThreshold							sysname				= N'40 seconds',
+	
+	@AGSyncCheckIterationCount				int					= 8, 
+	@AGSyncCheckDelayBetweenChecks			sysname				= N'1800 milliseconds',
+	@ExcludeAnomolousSyncDeviations			bit					= 0,    -- Primarily for Ghosted Records Cleanup... 
+	
 	@EmailSubjectPrefix						nvarchar(50)		= N'[Data Synchronization Problems] ',
 	@MailProfileName						sysname				= N'General',	
 	@OperatorName							sysname				= N'Alerts',	
@@ -152,6 +239,21 @@ AS
 	IF @rtoSeconds < 2 OR @rpoSeconds < 2 BEGIN 
 		RAISERROR(N'@RPOThreshold and @RTOThreshold values can not be set to less than 2 seconds.', 16, 1);
 		RETURN -10;
+	END;
+
+	-- translate @AGSyncCheckDelayBetweenChecks into waitfor value. 
+	DECLARE @waitFor sysname;
+	SET @vectorError = NULL;
+
+	EXEC dbo.[translate_vector_delay] 
+		@Vector = @AGSyncCheckDelayBetweenChecks, 
+		@ParameterName = N'@AGSyncCheckDelayBetweenChecks', 
+		@Output = @waitFor OUTPUT, 
+		@Error = @vectorError OUTPUT;
+
+	IF @vectorError IS NOT NULL BEGIN 
+		RAISERROR(@vectorError, 16, 1);
+		RETURN -20;
 	END;
 
     ----------------------------------------------
@@ -357,7 +459,11 @@ AS
 	
 		SET @currentAGName = N'';
 		SET @currentAGId = NULL;
-		EXEC master.sys.sp_executesql N'SELECT @currentAGName = ag.[name], @currentAGId = ag.group_id FROM sys.availability_groups ag INNER JOIN sys.availability_replicas ar ON ag.group_id = ar.group_id INNER JOIN sys.databases d ON ar.replica_id = d.replica_id WHERE d.[name] = @currentAGdDatabase;', N'@currentAGdDatabase sysname, @currentAGName sysname OUTPUT, @currentAGId uniqueidentifier OUTPUT', @currentAGdDatabase = @currentAGdDatabase, @currentAGName = @currentAGName OUTPUT, @currentAGId = @currentAGId OUTPUT;
+		EXEC sys.sp_executesql N'SELECT @currentAGName = ag.[name], @currentAGId = ag.group_id FROM sys.availability_groups ag INNER JOIN sys.availability_replicas ar ON ag.group_id = ar.group_id INNER JOIN sys.databases d ON ar.replica_id = d.replica_id WHERE d.[name] = @currentAGdDatabase;', 
+			N'@currentAGdDatabase sysname, @currentAGName sysname OUTPUT, @currentAGId uniqueidentifier OUTPUT', 
+			@currentAGdDatabase = @currentAGdDatabase, 
+			@currentAGName = @currentAGName OUTPUT, 
+			@currentAGId = @currentAGId OUTPUT;
 
 		IF NOT EXISTS (SELECT NULL FROM @processedAgs WHERE agname = @currentAGName) BEGIN
 		
@@ -424,22 +530,24 @@ AS
 	CLOSE ag_checker;
 	DEALLOCATE ag_checker;
 
-CREATE TABLE [#metrics] (
-	[row_id] int IDENTITY(1, 1) NOT NULL,
-	[database_name] sysname NOT NULL,
-	[iteration] int NOT NULL,
-	[timestamp] datetime NOT NULL,
-	[synchronization_delay (RPO)] decimal(20, 2) NOT NULL,
-	[recovery_time (RTO)] decimal(20, 2) NOT NULL,
-	-- raw data: 
-	[redo_queue_size] decimal(20, 2) NULL,
-	[redo_rate] decimal(20, 2) NULL,
-	[primary_last_commit] datetime NULL,
-	[secondary_last_commit] datetime NULL
-);
+	CREATE TABLE [#metrics] (
+		[row_id] int IDENTITY(1, 1) NOT NULL,
+		[database_name] sysname NOT NULL,
+		[iteration] int NOT NULL,
+		[timestamp] datetime NOT NULL,
+		[synchronization_delay (RPO)] decimal(20, 2) NOT NULL,
+		[recovery_time (RTO)] decimal(20, 2) NOT NULL,
+		-- raw data: 
+		[redo_queue_size] decimal(20, 2) NULL,
+		[redo_rate] decimal(20, 2) NULL,
+		[primary_last_commit] datetime NULL,
+		[secondary_last_commit] datetime NULL, 
+		[ignore_rpo_as_anomalous] bit NOT NULL CONSTRAINT DF_metrics_anomalous DEFAULT (0)
+	);
+
 
 	DECLARE @iterations int = 1; 
-	WHILE @iterations < 6 BEGIN
+	WHILE @iterations < @AGSyncCheckIterationCount BEGIN
 
 		WITH [metrics] AS (
 			SELECT
@@ -502,9 +610,48 @@ CREATE TABLE [#metrics] (
 			[primary] p 
 			INNER JOIN [secondary] s ON p.[database_name] = s.[database_name];
 
-		WAITFOR DELAY '00:00:01.500';
+		WAITFOR DELAY @waitFor;
 
 		SET @iterations += 1;
+	END;
+
+	IF @ExcludeAnomolousSyncDeviations = 1 BEGIN 
+
+		WITH derived AS ( 
+
+			SELECT 
+				[database_name],
+				CAST(MAX([synchronization_delay (RPO)]) AS decimal(20, 2)) [max],
+				CAST(AVG([synchronization_delay (RPO)]) AS decimal(20, 2)) [mean], 
+				CAST(STDEV([synchronization_delay (RPO)]) AS decimal(20, 2)) [deviation]
+			FROM 
+				[#metrics] 
+			GROUP BY 
+				[database_name]
+
+		), 
+		db_iterations AS ( 
+	
+			SELECT 
+				(
+					SELECT TOP 1 x.row_id 
+					FROM [#metrics] x 
+					WHERE x.[synchronization_delay (RPO)] = d.[max] AND [x].[database_name] = d.[database_name] 
+					ORDER BY x.[synchronization_delay (RPO)] DESC
+				) [row_id]
+			FROM 
+				[derived] d
+			WHERE 
+				d.mean - d.[deviation] < 0 -- biz-rule - only if/when deviation 'knocks everything' negative... 
+				AND d.[max] > ([d].[mean] + d.[deviation] + ABS([d].[mean] - d.[deviation]))
+		)
+
+		UPDATE m 
+		SET 
+			m.[ignore_rpo_as_anomalous] = 1 
+		FROM 
+			[#metrics] m 
+			INNER JOIN [db_iterations] x ON m.[row_id] = x.[row_id];
 	END;
 
 	WITH violations AS ( 
@@ -516,6 +663,7 @@ CREATE TABLE [#metrics] (
 				SELECT 
 					[x].[iteration] [@iteration], 
 					[x].[timestamp] [@timestamp],
+					[x].[ignore_rpo_as_anomalous],
 					[x].[synchronization_delay (RPO)] [rpo], 
 					[x].[recovery_time (RTO)] [rto],
 					[x].[redo_queue_size], 
@@ -523,13 +671,17 @@ CREATE TABLE [#metrics] (
 					[x].[primary_last_commit], 
 					[x].[secondary_last_commit]
 				FROM 
-					[#metrics] x WHERE x.[database_name] = m.[database_name]
+					[#metrics] x 
+				WHERE 
+					x.[database_name] = m.[database_name]
 				ORDER BY 
 					[x].[row_id] 
 				FOR XML PATH('detail'), ROOT('details')
 			) AS xml) [raw_data]
 		FROM 
 			[#metrics] m
+		WHERE 
+			m.[ignore_rpo_as_anomalous] = 0  -- note: these don't count towards rpo values - but they ARE included in serialized xml output (for review/analysis purposes). 
 		GROUP BY
 			[database_name]
 	) 
