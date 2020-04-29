21d6ee1d2c063d709e453071f47f6d29d03b0365
Michael K. Campbell <mike@overachiever.net>
2020-04-29 16:51:00 -0700

    S4-376: print_long_strings now breaks on CRLFs
    cleaner/more-obvious 'chunking rules' implemented.

--- S4 Tools/print_long_string.sql
+++ S4 Tools/print_long_string.sql
@@ -5,33 +5,14 @@
 			interesting... what about using the \ character as a line-continuation character? 
 
 
+	Obviously, PRINT is the best way to print... 
+	Only, sometimes you'll need to PRINT something with a LENGTH > 4K characters. Only, PRINT terminates at the first 4K chars - period. 
 
-	Obviously, PRINT 'xyz' is how to print. 
-	Only, sometimes you'll need to print something with a LENGTH > 4K characters. 
+	So, this sproc bypasses that - it just keeps spitting out ~4k chunks until the 'print' operation is done. 
 
-	That's what this sproc does. (It just keeps spitting out 4k chunks until the 'print' operation is done.) 
-
-
-    vNEXT: 
-        Add in a delimiter - or, at least, an OPTION for one - i.e., a BIT field only... 
-            which would basically work as follows: 
-                - instead of chunking at 4000 chars a pop, we chunk at 3996 (i.e., @chunkSize will have to be dynamically set in a variable based on @MarkBoundaries being 0 or 1
-                - if this is chunk number 0... and @MarkBoundaries = 1... do nothing BEFORE printing. 
-                - if @chunkNumber > 0 ... and we still have @chars to process... PRINT N'-- 4K characters reached by dbo.print_long_string' + @crlf + N''* /   (without spaces... sigh)... 
-                
-                - always end everything that we print out (except for the 'short-circuit if we don't have @chunkSize left)... 
-                    with a PRINT @substring + N'/ *'  (only... don't include spaces obviously - doing that here so it doesn't break comments).
+	NOTE: dbo.print_long_string 'terminates' ~4K chunks on the last CRLF before the 4K boundary ... so that we don't truncate outputs 'mid line'... 
 
 
-            that way we'd get something like the exact following: 
-
-... 
-... AND someValue = 1 A/*
--- 4K charactgers reached by dbo.print_long_string
-*/ND this is the rest of the text 
-... 
-.. 
-so that we're JUST inserting INLINE comments right into the middle of a statement and that it SHOULD (unless we're in the middle of inline comments (sigh)... be ignored... 
 
 */
 
@@ -49,23 +30,38 @@ AS
 
 	-- {copyright}
 
-	DECLARE @totalLen int;
-	SELECT @totalLen = LEN(@Input);
+	DECLARE @totalLength int = LEN(@Input); 
 
-	IF @totalLen < 4000 BEGIN 
+	DECLARE @currentLocation int = 1; -- NOT 0 based... 
+	DECLARE @chunk nvarchar(4000);
+
+	DECLARE @crlf nchar(2) = NCHAR(13) + NCHAR(10);
+	DECLARE @crlfLocation int;
+
+	IF @totalLength <= 4000 BEGIN 
 		PRINT @Input;
 		RETURN 0; -- done
-	END 
+	END;	
 
-	DECLARE @chunkLocation int = 0;
-	DECLARE @substring nvarchar(4000);
-
-	WHILE @chunkLocation <= @totalLen BEGIN 
-		SET @substring = SUBSTRING(@Input, @chunkLocation, 4000);
+	WHILE @currentLocation <= @totalLength BEGIN
+		SET @chunk = SUBSTRING(@Input, @currentLocation, 4001); -- final arg = POSITION (not number of chars to take).
 		
-		PRINT @substring;
+		IF LEN(@chunk) = 4000 BEGIN 
 
-		SET @chunkLocation += 4000;
+			SET @crlfLocation = CHARINDEX(REVERSE(@crlf), REVERSE(@chunk));  -- get the last (in chunk) crlf... 
+
+			IF @crlfLocation > 0 BEGIN 
+				SELECT @chunk = SUBSTRING(@chunk, 0, (LEN(@chunk) - @crlfLocation));
+			END;
+		END;
+
+		SET @currentLocation = @currentLocation + (LEN(@chunk));
+
+		IF LEFT(@chunk, 2) = @crlf BEGIN 
+			SET @chunk = RIGHT(@chunk, LEN(@chunk) - 2);
+		END;
+
+		PRINT @chunk;
 	END;
 
 	RETURN 0;
