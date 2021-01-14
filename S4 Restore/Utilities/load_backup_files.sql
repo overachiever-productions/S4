1ff5a01fbfb183669ed6330584640f218b9a8a5a
Michael K. Campbell <mike@overachiever.net>
2021-01-14 13:43:47 -0800

    S4-433: Interface rewrite for 'early LSN' fix
    BackupFinishDate from RESTORE HEADERONLY shows when a backup COMPLETES (whereas file-name shows when it starts).
    Now using COMPLETION time for filtering logic of finding/applying DIFF/LOG files instead of backup START times (timestamp from filename). This will correct issues with "LSN is too early to apply" errors.
    
    Cleaned up examples + provided high-level docs/overview of how old and NEW interfaces are supported.

--- S4 Restore/Utilities/load_backup_files.sql
+++ S4 Restore/Utilities/load_backup_files.sql
@@ -3,48 +3,74 @@
     NOTE: 
         - This sproc adheres to the PROJECT/REPLY usage convention.
 
+		- This sproc 
+			a. PRIMARILY exists for internal use/operations (in the form of providing a list of backups available for restore or CLEANUP. 
+				In this capacity, it is called/used by dbo.apply_logs, dbo.restore_backups, and ALSO by dbo.remove_backup_files (using the LIST mode). 
 
-	Sproc exists primarily for 1 reason: 
-		- to 'wrap' logic for grabbing a list of available backups... 
-		- so that this logic can be RE-USED multiple times (as needed) when running restore operations (so that we can look for NEWLY added files and such if/when restore operations take a long time to execute).
+			b. BUT, it was also DESIGNED to be used/called MANUALLY - for whatever reasons. 
+
+			Because of the above, there are, in essence 2 'overloads' or signatures that can be used when calling this sproc. 
+				For scenario (a) usages, S4 code will pass in @LastAppliedFinishTime - so that, if for example, we've restored a FULL (that took 64 minutes to create)
+						we make sure to start grabbing DIFF or T-LOGS > @CompletionTimeOfLastAppliedBackup ... (vs attempting to use time-stamp of filename - which represents backup start - i.e., 64 minutes EARLIER).
+
+				For scenario (b) usage (which was the 'legacy' signature), @LastAppliedFile can OPTIONALLY be passed in INSTEAD. 
+					In which case, this sproc will: 
+						i. load header details for the last-applied file. 
+						ii. bind the value of @LastAppliedFinishTime by derived info from header data... 
+					(Legacy behavior was to simply use the file-name timestamp of the backup file defined by @LastAppliedFile and grab anything > @thatTimestamp. 
+							This could/would/did lead to the dreaded "This LSN is too recent to apply... " errors... for which S4 code USED to try and 'loop' through the next N T-LOGs as a work-around).
+
+
+	SIGNATURES / EXAMPLES: 
 
         -- Expect PROJECTion as output:
                 EXEC dbo.load_backup_files 
                     @DatabaseToRestore = N'Billing', 
                     @SourcePath = N'D:\SQLBackups\Billing', 
-                    @Mode = N'FULL', 
-                    @LastAppliedFile = NULL;
+                    @Mode = N'FULL';
 
                 EXEC dbo.load_backup_files 
                     @DatabaseToRestore = N'Billing', 
                     @SourcePath = N'D:\SQLBackups\Billing', 
-                    @Mode = N'LIST', 
-                    @LastAppliedFile = NULL;
+                    @Mode = N'LIST';
 
 
-        -- Example of REPLY outputs - for all file-types... 
-			NOTE: these examples are busted - due to change from nvarchar, string, concatenated, lists, of, backups to ... xml instead.
+        -- Example of (iterative) REPLY outputs - for all file-types... 
 
 		        -- FULL:
 		        DECLARE @lastFile nvarchar(400) = NULL;
 		        DECLARE @output xml;
 		        EXEC dbo.load_backup_files @DatabaseToRestore = N'Billing', @SourcePath = N'D:\SQLBackups\Billing', @Mode = N'FULL', @LastAppliedFile = NULL, @Output = @output OUTPUT;
-		        SELECT @output [FULL BACKUP FILE];
+		        --SELECT @output [FULL BACKUP FILE];
 
-		        --SELECT @lastFile = @output;
+				SELECT @lastFile = @output.value('(/files/file/@file_name)[1]', 'sysname');
+				SELECT @lastFile [LastFullBackupToRestore];
 
 		        -- DIFF (if present):
                 SET @output = NULL;
 		        EXEC dbo.load_backup_files @DatabaseToRestore = N'Billing', @SourcePath = N'D:\SQLBackups\Billing', @Mode = N'DIFF', @LastAppliedFile = @lastFile, @Output = @output OUTPUT;
-		        SET @lastFile = ISNULL(NULLIF(@output,''), @lastFile);
-		        SELECT @lastFile [Last Applied (DIFF if present - otherwise FULL)]
+		        
+				IF @Output IS NOT NULL BEGIN 
+					SELECT @lastFile = @output.value('(/files/file/@file_name)[1]', 'sysname');
 
-
+					SELECT @lastFile [MostRecentDiffToApply];
+				END; ELSE BEGIN 
+					SELECT N'-' [NoDiffFoundForRestore];
+				END;
+				
 		        -- T-LOGs:
                 SET @output = NULL;
 		        EXEC dbo.load_backup_files @DatabaseToRestore = N'Billing', @SourcePath = N'D:\SQLBackups\Billing', @Mode = N'LOG', @LastAppliedFile = @lastFile, @Output = @output OUTPUT;
-		        SELECT * FROM dbo.[split_string](@output, N',', 1);
-		        GO
+		        
+				IF @Output IS NOT NULL BEGIN 
+					SELECT 
+						x.r.value('@file_name', 'sysname') [T-LogsToRestore]
+					FROM 
+						@output.nodes('//file') x(r)
+
+				END; ELSE BEGIN 
+					SELECT '-' [NoLogsFoundForRestore];
+				END;
 
 
 
@@ -63,6 +89,7 @@ CREATE PROC dbo.load_backup_files
 	@SourcePath					nvarchar(400), 
 	@Mode						sysname,				-- FULL | DIFF | LOG | LIST			-- where LIST = 'raw'/translated results.
 	@LastAppliedFile			nvarchar(400)			= NULL,	
+	@LastAppliedFinishTime		datetime				= NULL, 
 	@Output						xml						= N'<default/>'	    OUTPUT
 AS
 	SET NOCOUNT ON; 
@@ -77,7 +104,26 @@ AS
 		RAISERROR('Configuration Error: Invalid @Mode specified.', 16, 1);
 		SET @Output = NULL;
 		RETURN -1;
-	END 
+	END; 
+
+	IF @Mode IN (N'DIFF', N'LOG') BEGIN
+
+		IF @LastAppliedFinishTime IS NULL AND @LastAppliedFile IS NOT NULL BEGIN 
+			DECLARE @fullPath nvarchar(260) = dbo.[normalize_file_path](@SourcePath + N'\' + @LastAppliedFile);
+
+			EXEC dbo.load_header_details 
+				@BackupPath = @fullPath, 
+				@BackupDate = @LastAppliedFinishTime OUTPUT, 
+				@BackupSize = NULL, 
+				@Compressed = NULL, 
+				@Encrypted = NULL;
+		END;
+
+		IF @LastAppliedFile IS NULL BEGIN 
+			RAISERROR(N'Execution in ''DIFF'' or ''LOG'' Mode requires either a valid @LastAppliedFile or @LastAppliedFinishTime for filtering.', 16, 1);
+			RETURN -20;
+		END;
+	END;
 
 	DECLARE @results table ([id] int IDENTITY(1,1) NOT NULL, [output] varchar(500), [timestamp] datetime NULL);
 
@@ -169,7 +215,7 @@ AS
 
 	IF UPPER(@Mode) = N'DIFF' BEGIN 
 		-- start by deleting since the most recent file processed: 
-		DELETE FROM @orderedResults WHERE id <= (SELECT id FROM @orderedResults WHERE [output] = @LastAppliedFile);
+		DELETE FROM @orderedResults WHERE [timestamp] <= @LastAppliedFinishTime;
 
 		-- now dump everything but the most recent DIFF - if there is one: 
 		IF EXISTS(SELECT NULL FROM @orderedResults WHERE [output] LIKE 'DIFF%')
@@ -180,7 +226,7 @@ AS
 
 	IF UPPER(@Mode) = N'LOG' BEGIN
 		
-		DELETE FROM @orderedResults WHERE id <= (SELECT MIN(id) FROM @orderedResults WHERE [output] = @LastAppliedFile);
+		DELETE FROM @orderedResults WHERE [timestamp] <= @LastAppliedFinishTime;
 		DELETE FROM @orderedResults WHERE [output] NOT LIKE 'LOG%';
 	END;
 
