67a8ec6611a9adb2a11854f51c936739d7ef4f70
Michael K. Campbell <mike@overachiever.net>
2024-04-25 13:45:17 -0700

    Minor tweaks/improvements and code-cleanup.

--- S4 Diagnostics/QueryStore/querystore_list_forced_plans.sql
+++ S4 Diagnostics/QueryStore/querystore_list_forced_plans.sql
@@ -5,19 +5,28 @@
 			- use the above to get a list of databases ... and remove those where querystore is not enabled. 
 			- create a #table for results - which'll also include [database_name] as a column... 
 			- tweak the SQL accordingly... 
-
-
-
-	vNEXT:
-		- Move this into admindb. 
-			- which means dynamic SQL... 
-		- switch last compile/execution to admindb.dbo.format_timespan? 
-		- dynamic details on ... failures or not @includeFailures ... 
-		- dynamic @includeObjectDetails. 
-
-
+					   			 		  
 	vNEXT:
 		- rather than object_id for statements from a sproc/udf/etc. that are forced, why not pull back the actual object name and/or object_id (name_here) or whatever.
+
+
+	vNEXT: 
+		- metrics/deviation: 
+			I JUST need the SSMS approach to DEVIATION for CPU/DURATION 
+				i.e., I do NOT NEED their 'stdev_xxx' column. 
+				NOR do I need their 'avg' column. 
+
+			Instead, I should replace 'stdev_xxx' and 'avg_xxx' WITH:
+				MAX(x)
+				AVG(x) 
+
+
+
+
+	SIGNATURE: 
+		admindb.dbo.[querystore_list_forced_plans] 
+			@OrderResultsBy = N'CPU', 
+			@ShowTimespanInsteadOfDates = 1;
 
 */
 
@@ -32,11 +41,22 @@ CREATE PROC dbo.[querystore_list_forced_plans]
 	@TargetDatabase						sysname		= NULL, 
 	@ShowTimespanInsteadOfDates			bit			= 0, 
 	@IncludeObjectDetails				bit			= 0, 
+	@OrderResultsBy						sysname		= N'DURATION',			-- { DURATION_DEVIATION | CPU_DEVIATION | DURATION | CPU | ERRORS | PLAN_COUNT } 
 	@IncludeScriptDirectives			bit			= 1
 AS
     SET NOCOUNT ON; 
 
 	-- {copyright}
+
+	SET @ShowTimespanInsteadOfDates = ISNULL(@ShowTimespanInsteadOfDates, 0);
+	SET @IncludeObjectDetails = ISNULL(@IncludeObjectDetails, 0);
+	SET @IncludeScriptDirectives = ISNULL(@IncludeScriptDirectives, 1);
+	SET @OrderResultsBy = ISNULL(NULLIF(@OrderResultsBy, N''), N'DURATION');
+
+	IF UPPER(@OrderResultsBy) NOT IN (N'DURATION_DEVIATION', N'CPU_DEVIATION', N'DURATION', N'CPU', N'ERRORS', N'PLAN_COUNT') BEGIN 
+		RAISERROR(N'Allowed values for @OrderResultsBy are { DURATION_DEVIATION | CPU_DEVIATION | DURATION | CPU | ERRORS | PLAN_COUNT }.', 16, 1);
+		RETURN -10;
+	END;
 
 	IF @TargetDatabase IS NULL BEGIN 
 		EXEC dbo.[get_executing_dbname] @ExecutingDBName = @TargetDatabase OUTPUT;
@@ -51,29 +71,23 @@ AS
 		RAISERROR(N'The specified @TargetDatabase: [%s] does NOT exist OR Query Store is NOT enabled within %s.', 16, 1, @TargetDatabase, @TargetDatabase);
 		RETURN -10;
 	END;
-	
-	DECLARE @times nvarchar(MAX) = N'CAST([p].[last_compile_start_time] AS datetime) [last_compile],
-		CAST([p].[last_execution_time] AS datetime) [last_execution],';
 
-	IF @ShowTimespanInsteadOfDates = 1 BEGIN 
-		SET @times = N'admindb.dbo.format_timespan(DATEDIFF(MILLISECOND, [p].[last_compile_start_time], GETDATE())) [last_compile],
-		admindb.dbo.format_timespan(DATEDIFF(MILLISECOND,[p].[last_execution_time], GETDATE())) [last_execution],';
-	END;
-
-	DECLARE @objectDetails nvarchar(MAX) = N'
-		[q].[object_id] [object_id],
-		ISNULL(OBJECT_NAME([q].[object_id]), '''') [object_name],';
-
-	IF @IncludeObjectDetails = 0 
-		SET @objectDetails = N'';
-
-	DECLARE @scriptDirectives nvarchar(MAX) = N', 
-		N'''' [ ], 
-		N''EXEC [{db_name}].sys.sp_query_store_unforce_plan @query_id = '' + CAST([p].[query_id] AS sysname) + N'', @plan_id = '' + CAST([p].[plan_id] AS sysname) + N'';'' [remove_script], 
-		N''EXEC [{db_name}].sys.sp_query_store_force_plan @query_id = '' + CAST([p].[query_id] AS sysname) + N'', @plan_id = '' + CAST([p].[plan_id] AS sysname) + N'';'' [force_script] ';
-
-	IF @IncludeScriptDirectives = 0 
-		SET @scriptDirectives = N'';
+	CREATE TABLE #forced_plans (
+		[query_id] bigint NOT NULL,
+		[plan_id] bigint NOT NULL,
+		[query_text] nvarchar(max) NULL,
+		[query_plan] xml NULL,
+		[forcing_type] nvarchar(60) NULL,
+		[plan_count] int NOT NULL, 
+		[last_compile] datetime NULL,
+		[last_execution] datetime NULL,
+		[object_id] bigint NULL,
+		[object_name] sysname NOT NULL,
+		[failures] bigint NOT NULL,
+		[last_failure] sysname NULL,
+		[remove_script] nvarchar(331) NULL,
+		[force_script] nvarchar(329) NULL
+	);
 
 	DECLARE @sql nvarchar(MAX) = N'SELECT
 		[p].[query_id] [query_id],
@@ -81,23 +95,143 @@ AS
 		[qt].[query_sql_text] [query_text],
 		TRY_CAST([p].[query_plan] AS xml) [query_plan],
 		[p].[plan_forcing_type_desc] [forcing_type],
-		{times}{object_details}
+		(SELECT COUNT(*) FROM [sys].[query_store_plan] [x] WHERE x.query_id = [p].query_id) [plan_count],
+		CAST([p].[last_compile_start_time] AS datetime) [last_compile],
+		CAST([p].[last_execution_time] AS datetime) [last_execution],
+		[q].[object_id] [object_id],
+		ISNULL(OBJECT_NAME([q].[object_id]), '''') [object_name],
 		[p].[force_failure_count] [failures],
-		[p].[last_force_failure_reason_desc] [last_failure]{script_directives}
+		[p].[last_force_failure_reason_desc] [last_failure],
+		N''EXEC [{db_name}].sys.sp_query_store_unforce_plan @query_id = '' + CAST([p].[query_id] AS sysname) + N'', @plan_id = '' + CAST([p].[plan_id] AS sysname) + N'';'' [remove_script], 
+		N''EXEC [{db_name}].sys.sp_query_store_force_plan @query_id = '' + CAST([p].[query_id] AS sysname) + N'', @plan_id = '' + CAST([p].[plan_id] AS sysname) + N'';'' [force_script]
 	FROM
 		[{db_name}].[sys].[query_store_plan] [p]
-		JOIN [{db_name}].[sys].[query_store_query] [q] ON [q].[query_id] = [p].[query_id]
-		JOIN [{db_name}].[sys].[query_store_query_text] [qt] ON [q].[query_text_id] = [qt].[query_text_id]
+		INNER JOIN [{db_name}].[sys].[query_store_query] [q] ON [q].[query_id] = [p].[query_id]
+		INNER JOIN [{db_name}].[sys].[query_store_query_text] [qt] ON [q].[query_text_id] = [qt].[query_text_id]
 	WHERE
 		[p].[is_forced_plan] = 1; ';
 
-	SET @sql = REPLACE(@sql, N'{times}', @times);
-	SET @sql = REPLACE(@sql, N'{object_details}', @objectDetails);
-	SET @sql = REPLACE(@sql, N'{script_directives}', @scriptDirectives);
 	SET @sql = REPLACE(@sql, N'{db_name}', @TargetDatabase);
 
+	INSERT INTO [#forced_plans] (
+		[query_id],
+		[plan_id],
+		[query_text],
+		[query_plan],
+		[forcing_type],
+		[plan_count],
+		[last_compile],
+		[last_execution],
+		[object_id],
+		[object_name],
+		[failures],
+		[last_failure],
+		[remove_script],
+		[force_script]
+	)
 	EXEC sys.sp_executesql 
 		@sql; 
+
+	SET @sql = N'SELECT 
+		[f].plan_id, 
+
+		CAST(AVG([rs].[avg_duration]) as decimal(24,2)) [avg_duration], 
+		CAST(MAX([rs].[avg_duration]) as decimal(24,2)) [max_duration],
+
+		-- Deviation Logic 100% STOLEN from SSMS "Queries with High Variation" Report:
+		ISNULL(ROUND(CONVERT(float, (SQRT(SUM([rs].[stdev_duration] * [rs].[stdev_duration] * [rs].[count_executions]) / NULLIF(SUM([rs].[count_executions]), 0)) * SUM([rs].[count_executions])) / NULLIF(SUM([rs].[avg_duration] * [rs].[count_executions]), 0)), 2), 0) [variation_duration],
+		
+		CAST(AVG([rs].[avg_cpu_time]) as decimal(24,2)) [avg_cpu_time], 
+		CAST(MAX([rs].[avg_cpu_time]) as decimal(24,2)) [max_cpu_time],
+		
+		-- Ditto (except copy/paste/tweak to find variation based on cpu_usage): 
+		ISNULL(ROUND(CONVERT(float, (SQRT(SUM([rs].[stdev_cpu_time] * [rs].[stdev_cpu_time] * [rs].[count_executions]) / NULLIF(SUM([rs].[count_executions]), 0)) * SUM([rs].[count_executions])) / NULLIF(SUM([rs].[avg_cpu_time] * [rs].[count_executions]), 0)), 2), 0) [variation_cpu_time]
+	FROM 
+		[#forced_plans] [f]
+		INNER JOIN [{db_name}].[sys].[query_store_runtime_stats] [rs] ON [f].[plan_id] = [rs].[plan_id]
+	GROUP BY 
+		[f].[plan_id]; ';
+
+	SET @sql = REPLACE(@sql, N'{db_name}', @TargetDatabase);
+
+	CREATE TABLE #metrics (
+		[plan_id] bigint NOT NULL,
+		[avg_duration] float NOT NULL, 
+		[max_duration] float NOT NULL, 
+		[variation_duration] float NOT NULL, 
+		[avg_cpu_time] float NOT NULL, 
+		[max_cpu_time] float NOT NULL, 
+		[variation_cpu_time] float NOT NULL
+	);
+
+	INSERT INTO [#metrics] (
+		[plan_id],
+		[avg_duration],
+		[max_duration],
+		[variation_duration],
+		[avg_cpu_time],
+		[max_cpu_time],
+		[variation_cpu_time]
+	)
+	EXEC sp_executesql 
+		@sql;
+
+	SET @sql = N'SELECT 
+	[f].[query_id],
+	[f].[plan_id],
+	[f].[query_text],
+	[f].[query_plan],
+	[f].[forcing_type],
+	[f].[plan_count],
+	{times}{options}
+	[f].[failures],
+	[f].[last_failure],
+	[m].[avg_duration],
+	[m].[max_duration],
+	[m].[variation_duration],
+	[m].[avg_cpu_time],
+	[m].[max_cpu_time],
+	[m].[variation_cpu_time], 
+	N'''' [ ],
+	[f].[remove_script],
+	[f].[force_script]
+FROM 
+	[#forced_plans] [f]
+	LEFT OUTER JOIN [#metrics] [m] ON [f].[plan_id] = [m].[plan_id]
+ORDER BY 
+	{order_by} DESC; '; 
+
+	DECLARE @times nvarchar(MAX) = N'[f].[last_compile],
+	[f].[last_execution], ';
+
+	IF @ShowTimespanInsteadOfDates = 1 BEGIN 
+		SET @times = N'admindb.dbo.format_timespan(DATEDIFF(MILLISECOND, [f].[last_compile], GETDATE())) [last_compile],
+	admindb.dbo.format_timespan(DATEDIFF(MILLISECOND,[f].[last_execution], GETDATE())) [last_execution],';
+	END;
+
+	DECLARE @options nvarchar(MAX) = N''; 
+	IF @IncludeObjectDetails = 1 BEGIN 
+		SET @options = NCHAR(13) + NCHAR(10) + NCHAR(9) + N'[f].[object_id],
+	[f].[object_name],'
+	END;
+
+	DECLARE @orderBy nvarchar(MAX);
+	SET @orderBy = CASE UPPER(@OrderResultsBy)
+		WHEN N'DURATION_DEVIATION' THEN N'[m].[variation_duration]'
+		WHEN N'CPU_DEVIATION' THEN N'[m].[variation_cpu_time]'
+		WHEN N'DURATION' THEN N'[m].[avg_duration]'
+		WHEN N'CPU' THEN N'[m].[avg_cpu_time]'
+		WHEN N'ERRORS' THEN N'[f].[failures]'
+		WHEN N'PLAN_COUNT' THEN N'[f].[plan_count]'
+		ELSE N'[m].[variation_duration]' 
+	END;
+
+	SET @sql = REPLACE(@sql, N'{times}', @times);
+	SET @sql = REPLACE(@sql, N'{options}', @options);
+	SET @sql = REPLACE(@sql, N'{order_by}', @orderBy);
+
+	EXEC sys.sp_executesql 
+		@sql;
 
 	RETURN 0; 
 GO
--- S4 Diagnostics/list_database_details.sql
+++ S4 Diagnostics/list_database_details.sql
@@ -68,37 +68,105 @@ AS
     --      d) version-enabled (i.e, 2008R2 - 2019 etc... )
     --      e) include VLF counts as well, etc. (min t-log size and everything - but as an @Include blah)
 
+									-------------------------------------------------------------------------------
+									-- Database basics/File Placement/etc
+									SELECT DB_NAME([database_id])AS [Database Name], 
+										   [file_id], name, physical_name, type_desc, state_desc, 
+										   CONVERT( bigint, size/128.0) AS [Total Size in MB]
+									FROM sys.master_files
+									WHERE [database_id] > 4 
+									AND [database_id] <> 32767
+									OR [database_id] = 2
+									ORDER BY [Total Size in MB] DESC;
 
-	-------------------------------------------------------------------------------
-	-- Database basics/File Placement/etc
-	SELECT DB_NAME([database_id])AS [Database Name], 
-		   [file_id], name, physical_name, type_desc, state_desc, 
-		   CONVERT( bigint, size/128.0) AS [Total Size in MB]
-	FROM sys.master_files
-	WHERE [database_id] > 4 
-	AND [database_id] <> 32767
-	OR [database_id] = 2
-	ORDER BY [Total Size in MB] DESC;
+									----------------------------------------------------------------
+									-- overview of databases:
 
-	----------------------------------------------------------------
-	-- overview of databases:
+									SELECT
+										db.[name] [db_name], 
+										CONVERT(decimal(20,2), sizes.size * 8.0 / 1024) [db_size_MB],
+										logsize.log_size [log_size_MB],
+										CASE 
+											WHEN logsize.log_size = 0 THEN -1.0
+											WHEN logused.log_used = 0 THEN 0.0
+											ELSE CAST(((logused.log_used / logsize.log_size) * 100.0) AS decimal(5,2))
+										END [%_log_used],
+										db.recovery_model_desc [RecoveryModel],
+										db.page_verify_option_desc [Page Verify],
+										sizes.Files [NumberOfNonLogFiles],
+										-- TODO: addin max-size/Growth (but that's hard cuz of > 1 FILE option)
+										-- TODO: add % of data files used. 
+										(SELECT COUNT(database_id) FROM master.sys.master_files WHERE [type] NOT IN (0,1) AND database_id = db.database_id) [FSorFTIFiles],
+										db.[compatibility_level]
+									FROM 
+										sys.databases db
+										LEFT OUTER JOIN (SELECT instance_name [db_name], CAST((cntr_value / 1024.0) AS decimal(20,2)) [log_size] FROM sys.dm_os_performance_counters WHERE counter_name LIKE 'Log File(s) Size %') logsize ON db.[name] = logsize.[db_name]
+										LEFT OUTER JOIN (SELECT instance_name [db_name], CAST((cntr_value / 1024.0) AS decimal(20,2)) [log_used] FROM sys.dm_os_performance_counters WHERE counter_name LIKE 'Log File(s) Used %') logused ON db.[name] = logused.[db_name]
+										LEFT OUTER JOIN (
+											SELECT	database_id, SUM(size) size, COUNT(database_id) [Files] FROM sys.master_files WHERE [type] = 0 GROUP BY database_id
+										) sizes ON db.database_id = sizes.database_id
+									ORDER BY 
+										sizes.size DESC;
 
+
+									-- include this as a 'feature' above... 
+
+									-- Bad/Potentially-Problematic databases:
+									SELECT 
+										name,
+										is_read_only,
+										is_auto_close_on,
+										is_auto_shrink_on,
+										is_parameterization_forced, 
+										is_auto_create_stats_on, 
+										is_auto_update_stats_async_on,
+										snapshot_isolation_state_desc,
+										is_read_committed_snapshot_on
+									FROM master.sys.databases 
+									WHERE 
+										is_read_only = 1 OR
+										is_auto_shrink_on = 1 OR
+										is_auto_close_on = 1 OR
+										is_parameterization_forced = 1 OR
+										is_auto_create_stats_on = 0 OR
+										is_auto_update_stats_async_on = 1 OR
+										(snapshot_isolation_state_desc = 'ON' AND name NOT IN ('master','msdb')) OR
+										is_read_committed_snapshot_on = 1;  
+
+
+
+	-----------------------------------------------------------------------------------------------------------------------------------------------------
+	-- This is 'getting there':
+	--	todo: 
+	--		a. collapse file-counts into a single column: [file_counts (data):(log):(fs):(fti)] ... with results/values like: 1:1:0:0 or 2:2:1:0 (where 2 log files would be a problem).
+	--		b. create a [problems] (or maybe [smells]? column where I identify things like: 
+	--			- > 1x log file (MULTIPLE_LOG_FILEs)
+	--			- AUTO_CLOSE_ON and AUTO_SHINK_ON
+	--			- similar problems iwth stats... and the likes. 
+	--		c. a 'state' column as well... 
+	--			- offline/Readonly (OFFLINE, READONLY)
+
+	-- and with b, c... there can be MORE than one value in these columns/rows - i.e. if a DB has > 1 log file AND is auto-close AND has X as problem, I'd expect it to be "MULTIPLE_LOG_FILES, AUTO_CLOSE_ON, PROB_X", etc.)
+	-----------------------------------------------------------------------------------------------------------------------------------------------------
 	SELECT
-		db.[name] [db_name], 
-		CONVERT(decimal(20,2), sizes.size * 8.0 / 1024) [db_size_MB],
-		logsize.log_size [log_size_MB],
+		db.[name] [database_name], 
+		db.[compatibility_level],
+		db.recovery_model_desc [recovery_model],
+		db.page_verify_option_desc [page_verify],
+		db.[is_read_committed_snapshot_on] [rcsi_enabled],
+		CAST(((sizes.size * 8.0 / 1024.0) / 1024.0) AS decimal(24,2)) [db_size (GB)],
+		CAST((logsize.log_size / 1024.0) AS decimal(24,2)) [log_size (GB)],
 		CASE 
 			WHEN logsize.log_size = 0 THEN -1.0
 			WHEN logused.log_used = 0 THEN 0.0
 			ELSE CAST(((logused.log_used / logsize.log_size) * 100.0) AS decimal(5,2))
 		END [%_log_used],
-		db.recovery_model_desc [RecoveryModel],
-		db.page_verify_option_desc [Page Verify],
-		sizes.Files [NumberOfNonLogFiles],
+
+		sizes.Files [#_of_data_files],
+		(SELECT COUNT(database_id) FROM master.sys.master_files WHERE [type] IN (1) AND [database_id] = db.[database_id]) [#_of_log_files],
 		-- TODO: addin max-size/Growth (but that's hard cuz of > 1 FILE option)
 		-- TODO: add % of data files used. 
-		(SELECT COUNT(database_id) FROM master.sys.master_files WHERE [type] IN (0,2) AND database_id = db.database_id) [FSorFTIFiles],
-		db.[compatibility_level]
+		(SELECT COUNT(database_id) FROM master.sys.master_files WHERE [type] NOT IN (0,1) AND database_id = db.database_id) [#_of_filestream_or_fti_files]
 	FROM 
 		sys.databases db
 		LEFT OUTER JOIN (SELECT instance_name [db_name], CAST((cntr_value / 1024.0) AS decimal(20,2)) [log_size] FROM sys.dm_os_performance_counters WHERE counter_name LIKE 'Log File(s) Size %') logsize ON db.[name] = logsize.[db_name]
@@ -108,28 +176,3 @@ AS
 		) sizes ON db.database_id = sizes.database_id
 	ORDER BY 
 		sizes.size DESC;
-
-
-	-- include this as a 'feature' above... 
-
-	-- Bad/Potentially-Problematic databases:
-	SELECT 
-		name,
-		is_read_only,
-		is_auto_close_on,
-		is_auto_shrink_on,
-		is_parameterization_forced, 
-		is_auto_create_stats_on, 
-		is_auto_update_stats_async_on,
-		snapshot_isolation_state_desc,
-		is_read_committed_snapshot_on
-	FROM master.sys.databases 
-	WHERE 
-		is_read_only = 1 OR
-		is_auto_shrink_on = 1 OR
-		is_auto_close_on = 1 OR
-		is_parameterization_forced = 1 OR
-		is_auto_create_stats_on = 0 OR
-		is_auto_update_stats_async_on = 1 OR
-		(snapshot_isolation_state_desc = 'ON' AND name NOT IN ('master','msdb')) OR
-		is_read_committed_snapshot_on = 1;  
