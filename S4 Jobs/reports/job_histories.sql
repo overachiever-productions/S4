1354fb1fb0da79a739bdb4f06262c8b3d08d7bd2
Michael K. Campbell <mike@overachiever.net>
2025-12-30 09:30:01 -0800

    S4-89: Additional columns for/via dbo.job_histories()

--- S4 Jobs/reports/job_histories.sql
+++ S4 Jobs/reports/job_histories.sql
@@ -15,14 +15,18 @@ RETURNS TABLE AS
 RETURN 
 	WITH history AS (
 		SELECT 
-			j.[name] [job_name], 
-			h.[step_id], 
-			h.[step_name], 
-			msdb.dbo.[agent_datetime](h.[run_date], h.[run_time]) [run_time], 
-			DATEDIFF(SECOND, 0, STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(6), h.[run_duration]), 6), 5, 0, ':') ,3 , 0, ':')) [run_seconds]
+			[j].[name] [job_name], 
+			[h].[step_id], 
+			[h].[step_name], 
+			msdb.dbo.[agent_datetime]([h].[run_date], [h].[run_time]) [run_time], 			
+			DATEDIFF(SECOND, 0, STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(6), [h].[run_duration]), 6), 5, 0, ':') ,3 , 0, ':')) [run_seconds],
+			[h].[run_status],
+			[h].[sql_message_id],
+			[h].[sql_severity],
+			[h].[message]
 		FROM 
-			msdb.dbo.sysjobs j 
-			INNER JOIN msdb.dbo.[sysjobhistory] h ON [j].[job_id] = [h].[job_id]
+			msdb.dbo.sysjobs [j] 
+			INNER JOIN msdb.dbo.[sysjobhistory] [h] ON [j].[job_id] = [h].[job_id]
 	), 
 	facts AS ( 
 		SELECT 
@@ -31,14 +35,26 @@ RETURN
 			[h].[step_name],
 			[h].[run_time], 
 			DATENAME(WEEKDAY, [h].[run_time]) [weekday],
-			[h].[run_seconds]
+			[h].[run_seconds], 
+			[h].[run_status],
+			[h].[sql_message_id],
+			[h].[sql_severity],
+			[h].[message]
 		FROM 
 			[history] h
 	)
 
-
 	SELECT 
-		* 
+		[job_name],
+		[step_id],
+		[step_name],
+		[run_time],
+		[weekday],
+		[run_seconds],
+		[run_status],
+		[sql_message_id],
+		[sql_severity],
+		[message] 
 	FROM 
 		[facts];
 GO
