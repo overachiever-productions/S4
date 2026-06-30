573d60bf0fd820151f49cd42a94f31cf90ac0d3f
overachiever-productions <mike@overachiever.net>
2026-06-30 13:40:58 -0700

    S4-445: Overhaul/Rewrite of "bounding" details. Overhaul of @jobs filter.
    BOUNDING no longer (stupidly) attempts to report on COMPLETE/INCOMPLETE like it did previously (and which was NOT working - in a spectacularly bad fashion).
    And @jobs can now be explicit matches and/or -Exclusions.

--- S4 Jobs/running_jobs.sql
+++ S4 Jobs/running_jobs.sql
@@ -1,8 +1,28 @@
 /*
 
+
+
+
     .CONVENTIONS:
 	- PROJECT or RETURN
 
+
+	.OUTPUTS 
+
+	.bounds 
+		Provides context/details on interaction between start and end of a job-step vs the @start and @end parameters (if/when provided). 
+		Specifically, there are 4 main outcomes possible: 
+			➡️⏹️ - Job Step was RUNNING before @start but ended before @end. 
+			➡️➡️  - Job Step was RUNNING before @start and continued running after @end.
+			⏹️➡️ - Job Step started after @start but continued running after @end.
+			⏹️⏹️ - Job Step started after @start and ended before @end.
+
+		NOTE: 
+			When no @start date is provided, then [bounds] will have an @start and @end of GETDATE(). 
+			Which means that we're looking for ANY jobs running "right now". 
+			And because they're running NOW (before @start) and will (presumably) still be running (after GETDATE()/@end)... 
+				then, any jobs actively running "right this second" will have a [bounds] of ➡️➡️
+				These jobs will not, however, have an [end_time] - because the job has NOT completed yet. 
 
 */
 
@@ -59,23 +79,23 @@ AS
 		[step_id] int NOT NULL,
 		[step_name] sysname NOT NULL,
 		[start_time] datetime NOT NULL,
-		[end_time] datetime NULL,
-		[completed] bit NULL
+		[end_time] datetime NULL
 	);
+
+	DECLARE @currentlyRunning bit = 0;
 
 	/*---------------------------------------------------------------------------------------------------------------------------------------------------
 	-- NO @start or @end - i.e., jobs running RIGHT NOW (or actively running jobs). 
 	---------------------------------------------------------------------------------------------------------------------------------------------------*/
 	IF @start IS NULL BEGIN
-		INSERT INTO [#RunningJobs] ( [job_name], [job_id], [step_name], [step_id], [start_time], [end_time], [completed])
+		INSERT INTO [#RunningJobs] ([job_name], [job_id], [step_name], [step_id], [start_time], [end_time])
 		SELECT 
 			j.[name] [job_name], 
 			ja.job_id,
 			js.[step_name] [step_name],
 			js.[step_id],
 			ja.[start_execution_date] [start_time], 
-			NULL [end_time], 
-			0 [completed]
+			NULL [end_time]
 		FROM 
 			msdb.dbo.[sysjobactivity] ja 
 			LEFT OUTER JOIN msdb.dbo.[sysjobhistory] jh ON [ja].[job_history_id] = [jh].[instance_id]
@@ -85,6 +105,11 @@ AS
 			[ja].[session_id] = (SELECT TOP (1) [session_id] FROM msdb.dbo.[syssessions] ORDER BY [agent_start_date] DESC) 
 			AND [ja].[start_execution_date] IS NOT NULL 
 			AND [ja].[stop_execution_date] IS NULL;
+
+		SET @currentlyRunning = 1;
+		--SELECT 
+		--	@start = GETDATE(), 
+		--	@end = GETDATE(); 
 
 		GOTO JOB_PREDICATES;
 	END;
@@ -133,15 +158,14 @@ AS
 			ends
 	)
 
-	INSERT INTO [#RunningJobs] ( [job_name], [job_id], [step_name], [step_id], [start_time], [end_time], [completed])
+	INSERT INTO [#RunningJobs] ([job_name], [job_id], [step_name], [step_id], [start_time], [end_time])
 	SELECT 
 		[j].[name] [job_name],
 		[n].[job_id], 
 		ISNULL([js].[step_name], [n].[step_name]) [step_name],
 		[n].[step_id],
 		[n].[start_time],
-		[n].[end_time], 
-		CASE WHEN [n].[next_job_step_id] = 0 THEN CAST(1 AS bit) ELSE CAST(0 AS bit) END [completed]
+		[n].[end_time]
 	FROM 
 		normalized n
 		LEFT OUTER JOIN msdb.dbo.[sysjobs] j ON [n].[job_id] = [j].[job_id] -- allow this to be NULL - i.e., if we're looking for a job that ran this morning at 2AM, it's better to see that SOMETHING ran other than that a Job that existed (and ran) - but has since been deleted - 'looks' like it didn't run.
@@ -150,17 +174,50 @@ AS
 		[n].[step_id] <> 0 AND ( 
 			[n].[start_time] <= @end 
 			AND [n].[end_time] >= @start
-		); 
+		);
 
+	/*---------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Job Predication:
+	---------------------------------------------------------------------------------------------------------------------------------------------------*/
 JOB_PREDICATES: 
-	-- Exclude any jobs specified: 
--- this needs to be a LIKE for + and - matches ... 
-	DELETE FROM [#RunningJobs] WHERE [job_name] IN (SELECT [result] FROM dbo.[split_string](@jobs, N',', 1));
+
+	CREATE TABLE #ts_cp_jobs (
+		[row_id] int IDENTITY(1,1) NOT NULL,
+		[job] sysname NOT NULL, 
+		[exclude] bit DEFAULT(0), 
+		PRIMARY KEY CLUSTERED ([exclude], [job])
+	);
+
+	IF @jobs IS NOT NULL BEGIN
+		INSERT INTO #ts_cp_jobs ([job], [exclude]) 
+		SELECT 
+			CASE WHEN [result] LIKE N'-%' THEN RIGHT([result], LEN([result]) -1) ELSE [result] END [job], 
+			CASE WHEN [result] LIKE N'-%' THEN 1 ELSE 0 END [exclude]	
+		FROM 
+			[dbo].[split_string](@jobs, N',', 1);
+
+		IF EXISTS (SELECT NULL FROM [#ts_cp_jobs] WHERE exclude = 0) BEGIN
+			DELETE [x]
+			FROM 
+				[#RunningJobs] [x]
+				INNER JOIN [#ts_cp_jobs] [j] ON [x].[job_name] NOT LIKE [j].[job] 
+			WHERE 
+				[j].[exclude] = 0;
+		END;
+
+		IF EXISTS (SELECT NULL FROM [#ts_cp_jobs] WHERE exclude = 1) BEGIN
+			DELETE [x]
+			FROM 
+				[#RunningJobs] [x]
+				INNER JOIN [#ts_cp_jobs] [j] ON [x].[job_name] LIKE [j].[job] 
+			WHERE 
+				[j].[exclude] = 1;
+		END;
+	END;
     
-	-- TODO: are there any expansions/details we want to join from the Jobs themselves at this point? (or any other history info?) 
-	
-	-----------------------------------------------------------------------------
-    -- Send output as XML if requested:
+	/*---------------------------------------------------------------------------------------------------------------------------------------------------
+	-- RETURN (vs PROJECT):
+	---------------------------------------------------------------------------------------------------------------------------------------------------*/
 	IF (SELECT dbo.is_xml_empty(@serialized_output)) = 1 BEGIN -- if @SerializedOutput has been EXPLICITLY initialized as NULL/empty... then REPLY...  
 
 		SELECT @serialized_output = (
@@ -170,8 +227,14 @@ JOB_PREDICATES:
 				[step_name],
 				[step_id],
 				[start_time],
-				CASE WHEN [completed] = 1 THEN [end_time] ELSE NULL END [end_time], 
-				CASE WHEN [completed] = 1 THEN 'COMPLETED' ELSE 'INCOMPLETE' END [job_status]
+				[end_time], 
+				CASE 
+					WHEN @currentlyRunning = 1 THEN N'➡️➡️'
+					WHEN [start_time] < @start AND [end_time] < @end THEN N'➡️⏹️'
+					WHEN [start_time] < @start AND [end_time] > @end THEN N'➡️➡️'
+					WHEN [start_time] >= @start AND [end_time] > @end THEN N'⏹️➡️'
+					ELSE N'⏹️⏹️'
+				END [bounds]
 			FROM 
 				[#RunningJobs] 
 			ORDER BY 
@@ -182,16 +245,23 @@ JOB_PREDICATES:
 		RETURN 0;
 	END;
 
-	-----------------------------------------------------------------------------
-	-- otherwise, project:
+	/*---------------------------------------------------------------------------------------------------------------------------------------------------
+	-- PROJECT:
+	---------------------------------------------------------------------------------------------------------------------------------------------------*/
 	SELECT 
 		[job_name],
         [job_id],
         [step_name],
 		[step_id],
         [start_time],
-		CASE WHEN [completed] = 1 THEN [end_time] ELSE NULL END [end_time], 
-		CASE WHEN [completed] = 1 THEN 'COMPLETED' ELSE 'INCOMPLETE' END [job_status]
+		[end_time],
+		CASE 
+			WHEN @currentlyRunning = 1 THEN N'➡️➡️'
+			WHEN [start_time] < @start AND [end_time] < @end THEN N'➡️⏹️'
+			WHEN [start_time] < @start AND [end_time] > @end THEN N'➡️➡️'
+			WHEN [start_time] >= @start AND [end_time] > @end THEN N'⏹️➡️'
+			ELSE N'⏹️⏹️'
+		END [bounds]
 	FROM 
 		[#RunningJobs]
 	ORDER BY 
